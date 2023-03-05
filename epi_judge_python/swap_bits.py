f669abb02f8360b4e44f253f8488f7c2ecfb09b5
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-03-05 16:02:07 +0530

    A1-python: Done with swap bits

--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -9,7 +9,10 @@ def clear_bit(n: int) -> int:
     return cleared_bit_n
 
 
-def swap_bits(x, i, j):
+def swap_bits_v1(x, i, j):
+    '''
+    My original solution O(n)
+    '''
     i_bit = x & (1 << i)
     j_bit = x & (1 << j)
     if i_bit and not j_bit:
@@ -22,9 +25,21 @@ def swap_bits(x, i, j):
         x |= (1 << i)
     return x
 
+def swap_bits_v2(x, i, j):
+    '''
+    Books O(1) solution
+    '''
+    if ((x >> i) & 1) != ((x >> j) & 1):
+        bit_mask = (1 << i) | (1 << j)
+        x ^= bit_mask
+    return x
+
+def swap_bits(x, i, j):
+    return swap_bits_v2(x, i, j)
 
 if __name__ == '__main__':
-    test_swap_bits.TestSwapBits(swap_bits, "Swap bits").run_tests()
+    test_swap_bits.TestSwapBits(swap_bits_v1, "My Swap bits").run_tests()
+    test_swap_bits.TestSwapBits(swap_bits_v2, "Book Swap bits").run_tests()
     exit(
         generic_test.generic_test_main('swap_bits.py', 'swap_bits.tsv',
                                        swap_bits))
