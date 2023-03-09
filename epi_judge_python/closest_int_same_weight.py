451ee9725fe5fe3f39e7a8c6057a0bfd488b0c8c
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-03-09 12:22:01 +0530

    A1-python: Done with finding closest int with same weight

--- epi_judge_python/closest_int_same_weight.py
+++ epi_judge_python/closest_int_same_weight.py
@@ -5,7 +5,7 @@ from count_bits import count_bits
 
 def closest_int_same_bit_count_v1(x: int) -> int:
     '''
-    Brute-force version
+    Brute-force version seems O(2^n)
     '''
     diff = 1
     wt = count_bits(x)
@@ -20,14 +20,36 @@ def closest_int_same_bit_count_v1(x: int) -> int:
         diff += 1
 
 
+def closest_int_same_bit_count_v2(x: int) -> int:
+    '''
+    Book O(n) solution
+    '''
+    i = 0
+    prev_bit = x & 1
+    n = x
+    while n:
+        i += 1
+        n >>= 1
+        curr_bit = n & 1
+        if prev_bit ^ curr_bit:
+            break
+        else:
+            prev_bit = curr_bit
+    if i > 0:
+        x ^= (1 << i)
+        x ^= (1 << (i - 1))
+    return x
+
+
+
 def closest_int_same_bit_count(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    return closest_int_same_bit_count_v2(x)
 
 
 if __name__ == '__main__':
     TestClosestBitSameWeight(closest_int_same_bit_count_v1).run_tests()
-    # exit(
-    #     generic_test.generic_test_main('closest_int_same_weight.py',
-    #                                    'closest_int_same_weight.tsv',
-    #                                    closest_int_same_bit_count))
+    TestClosestBitSameWeight(closest_int_same_bit_count_v2).run_tests()
+    exit(
+        generic_test.generic_test_main('closest_int_same_weight.py',
+                                       'closest_int_same_weight.tsv',
+                                       closest_int_same_bit_count))
