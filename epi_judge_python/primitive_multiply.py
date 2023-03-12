3a2b2bf23aa89bde3b206a484c60a290c8b7ee88
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-03-12 13:36:26 +0530

    A1-python: Doing addition using bitwise operations only

--- epi_judge_python/primitive_multiply.py
+++ epi_judge_python/primitive_multiply.py
@@ -1,10 +1,8 @@
 from test_framework import generic_test
-from tests.test_primitive_multiply import TestPrimitiveMultiply
-
-import pdb
+from tests.test_primitive_multiply import (TestPrimitiveMultiply,
+    TestPrimitiveAddition)
 
 def add_primitive(x: int, y: int) -> int:
-    # pdb.set_trace()
     sum = 0
     carry = 0
     i = 0
@@ -31,25 +29,20 @@ def multiply_v1(x: int, y: int) -> int:
             additives.append(y)
         y <<= 1
         x >>= 1
-    return sum(additives)
+    product = 0
+    if additives:
+        product = additives[0]
+        for rest_add in additives[1:]:
+            product = add_primitive(product, rest_add)
+    return product
 
 def multiply(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    return multiply_v1(x, y)
 
 
 if __name__ == '__main__':
-    # print(f"### {1} + {3} = {add_primitive(1, 3)}")
-    # print(f"### {2} + {3} = {add_primitive(2, 3)}")
-    # print(f"### {5} + {4} = {add_primitive(5, 4)}")
-    # print(f"### {0} + {1} = {add_primitive(0, 1)}")
-    # print(f"### {1} + {0} = {add_primitive(1, 0)}")
-    # print(f"### {21} + {9} = {add_primitive(21, 9)}")
-    for x in range(20):
-        for y in range(20):
-            print(f"### {x} + {y} = {add_primitive(x, y)}")
-            assert add_primitive(x, y) == (x + y), f"Failed for {x} + {y}"
+    TestPrimitiveAddition(add_primitive).run_tests()
     TestPrimitiveMultiply(multiply_v1).run_tests()
-    # exit(
-    #     generic_test.generic_test_main('primitive_multiply.py',
-    #                                    'primitive_multiply.tsv', multiply))
+    exit(
+        generic_test.generic_test_main('primitive_multiply.py',
+                                       'primitive_multiply.tsv', multiply))
--- epi_judge_python/tests/test_primitive_multiply.py
+++ epi_judge_python/tests/test_primitive_multiply.py
@@ -1,8 +1,13 @@
 from tests.test_base import TestBase
 
+class TestPrimitiveAddition(TestBase):
+    def test_addition(self):
+        for x in range(100):
+            for y in range(100):
+                assert self.solve(x, y) == (x + y), f"Failed for {x} + {y}"
+
 class TestPrimitiveMultiply(TestBase):
-    def test_example1(self):
-        for x in range(25):
-            for y in range(25):
-                # print(f"### {x} * {y} = {self.solve(x, y)}")
+    def test_multiplication(self):
+        for x in range(100):
+            for y in range(100):
                 assert self.solve(x, y) == x * y, f"Failed for {x} * {y}"
