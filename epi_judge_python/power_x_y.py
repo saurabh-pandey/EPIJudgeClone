3ed49bb1bcb57daf98db32782236fe43b723ded9
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-03-14 13:03:33 +0530

    A1-python: Implemented a recursive version of power function

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -1,5 +1,16 @@
 from test_framework import generic_test
+from tests.test_power_x_y import TestPowerXY
 
+def power_v1(x: float, y: int) -> float:
+    if x == 0.0:
+        return 0.0
+    if y == 0:
+        return 1.0
+    x_power_half = power_v1(x, y // 2)
+    if y & 1:
+        return x * x_power_half * x_power_half
+    else:
+        return x_power_half * x_power_half
 
 def power(x: float, y: int) -> float:
     # TODO - you fill in here.
@@ -7,5 +18,6 @@ def power(x: float, y: int) -> float:
 
 
 if __name__ == '__main__':
-    exit(generic_test.generic_test_main('power_x_y.py', 'power_x_y.tsv',
-                                        power))
+    TestPowerXY(power_v1).run_tests()
+    # exit(generic_test.generic_test_main('power_x_y.py', 'power_x_y.tsv',
+    #                                     power))
---
+++ epi_judge_python/tests/test_power_x_y.py
@@ -0,0 +1,17 @@
+from tests.test_base import TestBase
+
+import math
+
+class TestPowerXY(TestBase):
+    def test_example1(self):
+        for x in range(1, 6):
+            for y in range(1, 11):
+                assert self.solve(x, y) == pow(x, y)
+    
+    def test_example2(self):
+        start = 1.2
+        for i in range(1, 6):
+            for y in range(1, 11):
+                x = start + i
+                # print(f"{x}^{y} = {self.solve(x, y)}, {x ** y}, {math.isclose(self.solve(x, y), (x ** y))}")
+                assert math.isclose(self.solve(x, y), (x ** y))
