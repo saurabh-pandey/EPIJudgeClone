2c1f8b41c32dc0a33b1d326bff24bf5e2cb29376
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-03-15 13:08:46 +0530

    A1-python: Done with reversing digits

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -3,13 +3,10 @@ from tests.test_reverse_digits import TestReverseDigits
 
 import math
 
-import pdb
-
 def reverse_v1(x: int) -> int:
     '''
     Brute force version O(n)
     '''
-    # pdb.set_trace()
     if x == 0:
         return 0
     result = abs(x)
@@ -19,7 +16,6 @@ def reverse_v1(x: int) -> int:
         power = 10 ** (i - 1)
         digits[num_digits - i] = result // power
         result -= power * digits[num_digits - i]
-    # print(f"Extracted digits = {digits}")
     i = 0
     j = num_digits - 1
     while i < j:
@@ -28,7 +24,6 @@ def reverse_v1(x: int) -> int:
         digits[j] = temp
         i += 1
         j -= 1
-    # print(f"Reverse digits = {digits}")
     result = 0
     power = 10 ** (num_digits - 1)
     for d in digits:
@@ -36,13 +31,25 @@ def reverse_v1(x: int) -> int:
         power //= 10
     return -result if x < 0 else result
 
+def reverse_v2(x: int) -> int:
+    '''
+    Book's O(n) version
+    '''
+    result = 0
+    remaining_digits = abs(x)
+    while remaining_digits:
+        result = result * 10 + remaining_digits % 10
+        remaining_digits //= 10
+    return -result if x < 0 else result
 
 def reverse(x: int) -> int:
-    return reverse_v1(x)
+    # return reverse_v1(x)
+    return reverse_v2(x)
 
 
 if __name__ == '__main__':
     TestReverseDigits(reverse_v1).run_tests()
+    TestReverseDigits(reverse_v2).run_tests()
     exit(
         generic_test.generic_test_main('reverse_digits.py',
                                        'reverse_digits.tsv', reverse))
