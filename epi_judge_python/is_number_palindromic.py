b66e1b57a1e43668c12ac3731a384336eb06777d
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-03-16 12:41:28 +0530

    A1-python: Done with finding is number a palindrome

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -23,12 +23,30 @@ def is_palindrome_number_v1(x: int) -> bool:
         j -= 1
     return True
 
+def is_palindrome_number_v2(x: int) -> bool:
+    '''
+    Book's O(n) Time and O(1) space solution
+    '''
+    if x <= 0:
+        return x == 0
+    num_digits = math.floor(math.log10(x)) + 1
+    power = 10 ** (num_digits - 1)
+    for i in range(num_digits // 2):
+        if x % 10 != x // power:
+            return False
+        x %= power
+        x //= 10
+        power //= 100
+    return True
+
 def is_palindrome_number(x: int) -> bool:
-    return is_palindrome_number_v1(x)
+    # return is_palindrome_number_v1(x)
+    return is_palindrome_number_v2(x)
 
 
 if __name__ == '__main__':
     TestIsNumberPalindromic(is_palindrome_number_v1).run_tests()
+    TestIsNumberPalindromic(is_palindrome_number_v2).run_tests()
     exit(
         generic_test.generic_test_main('is_number_palindromic.py',
                                        'is_number_palindromic.tsv',
