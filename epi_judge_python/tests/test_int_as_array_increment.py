42b9af21707fc459097d5c3702aadf0f7fc41c74
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-03-22 11:36:42 +0530

    A1-python: Created a module to add common to all tests utility function

--- epi_judge_python/tests/test_int_as_array_increment.py
+++ epi_judge_python/tests/test_int_as_array_increment.py
@@ -1,21 +1,13 @@
 from tests.test_base import TestBase
 
+from tests.utils import int_as_array
+
 from typing import List
 
 
 class TestIntAsArrayIncrement(TestBase):
-    def _int_as_array(self, n: int) -> List[int]:
-        if n == 0:
-            return [0]
-        digits = []
-        while n:
-            digits.append(n % 10)
-            n //= 10
-        digits.reverse()
-        return digits
-
     def test_example1(self):
         for i in range(20001):
-            A = self._int_as_array(i)
-            expected = self._int_as_array(i + 1)
+            A = int_as_array.convert(i)
+            expected = int_as_array.convert(i + 1)
             assert self.solve(A) == expected
---
+++ epi_judge_python/tests/utils/int_as_array.py
@@ -0,0 +1,14 @@
+from typing import List
+
+def convert(n: int):
+    if n == 0:
+        return [0]
+    sign = 1 if n > 0 else -1
+    n = abs(n)
+    digits = []
+    while n:
+        digits.append(n % 10)
+        n //= 10
+    digits.reverse()
+    digits[0] *= sign
+    return digits
