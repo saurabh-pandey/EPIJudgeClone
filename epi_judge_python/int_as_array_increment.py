e775e6863317289822aa8b5f1d476c82653f51e5
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-03-22 10:46:51 +0530

    A1-python: Done with incrementing integer as an array

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -24,13 +24,30 @@ def plus_one_v1(A: List[int]) -> List[int]:
     res.reverse()
     return res
 
+def plus_one_v2(A: List[int]) -> List[int]:
+    '''
+    Book's in place O(n) version
+    '''
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i - 1] += 1
+    else:
+        if A[0] == 10:
+            A[0] = 1
+            A.append(0)
+    return A
+
 def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return plus_one_v1(A)
+    # return plus_one_v1(A)
+    return plus_one_v2(A)
 
 
 if __name__ == '__main__':
     TestIntAsArrayIncrement(plus_one_v1).run_tests()
+    TestIntAsArrayIncrement(plus_one_v2).run_tests()
     exit(
         generic_test.generic_test_main('int_as_array_increment.py',
                                        'int_as_array_increment.tsv', plus_one))
