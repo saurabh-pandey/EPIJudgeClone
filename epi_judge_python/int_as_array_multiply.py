2b9d10cb45e1ac165f0c398be182b691a878db53
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-03-23 12:18:02 +0530

    A1-python: Solved my version of multiplying two integer as arrays

--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -36,13 +36,41 @@ def add(num1: List[int], num2: List[int]) -> List[int]:
         larger_num[0] = 1
     return larger_num
 
+def multiply_v1(num1: List[int], num2: List[int]) -> List[int]:
+    '''
+    My O(n^2) version
+    '''
+    sign = -1 if (num1[0] * num2[0]) < 0 else 1
+    number1 = num1[:]
+    number2 = num2[:]
+    number1[0] = abs(number1[0])
+    number2[0] = abs(number2[0])
+    running_sum = []
+    for i, n1 in enumerate(reversed(number1)):
+        product = [0 for _ in range(i)]
+        carry = 0
+        for n2 in reversed(number2):
+            mult = n1 * n2 + carry
+            product.append(mult % 10)
+            carry = mult // 10
+        if carry:
+            product.append(carry)
+        product.reverse()
+        if not running_sum:
+            running_sum = [0 for _ in product]
+        running_sum = add(running_sum, product)
+    running_sum[0] *= sign
+    if all(n == 0 for n in running_sum):
+        running_sum = [0]
+    return running_sum
+
 def multiply(num1: List[int], num2: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    return multiply_v1(num1, num2)
 
 
 if __name__ == '__main__':
     TestIntAsArrayAdd(add).run_tests()
-    # exit(
-    #     generic_test.generic_test_main('int_as_array_multiply.py',
-    #                                    'int_as_array_multiply.tsv', multiply))
+    TestIntAsArrayMultiply(multiply_v1).run_tests()
+    exit(
+        generic_test.generic_test_main('int_as_array_multiply.py',
+                                       'int_as_array_multiply.tsv', multiply))
--- epi_judge_python/tests/test_int_as_array_multiply.py
+++ epi_judge_python/tests/test_int_as_array_multiply.py
@@ -1,31 +1,20 @@
 from tests.test_base import TestBase
 from tests.utils import int_as_array
 
-import pdb
-
 class TestIntAsArrayAdd(TestBase):
     def test_example1(self):
-        # pdb.set_trace()
-        # num1 = int_as_array.convert(0)
-        # num2 = int_as_array.convert(10)
-        # expected = int_as_array.convert(0 + 10)
-        # res = self.solve(num1, num2)
-        # print(f"{num1} + {num2} = {expected}, res = {res}")
-        for i in range(2000):
+        for i in range(1000):
             num1 = int_as_array.convert(i)
-            for j in range(2000):
+            for j in range(1000):
                 num2 = int_as_array.convert(j)
                 expected = int_as_array.convert(i + j)
-                # print(f"Starting add of {num1} + {num2}")
-                res = self.solve(num1, num2)
-                # print(f"{num1} + {num2} = {expected}, res = {res}")
-                assert res == expected
+                assert self.solve(num1, num2) == expected
 
 class TestIntAsArrayMultiply(TestBase):
     def test_example1(self):
-        for i in range(200):
+        for i in range(-250, 251):
             num1 = int_as_array.convert(i)
-            for j in range(200):
+            for j in range(-250, 251):
                 num2 = int_as_array.convert(j)
                 expected = int_as_array.convert(i * j)
                 assert self.solve(num1, num2) == expected
