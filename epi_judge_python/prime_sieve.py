d2d0d8be99b63f89efe74456bee4ec28a7e236bd
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-03-27 12:30:21 +0530

    A1-python: Implemented book's version of sieve algorithm

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -44,14 +44,36 @@ def generate_primes_v2(n: int) -> List[int]:
                 is_prime[multiple - 1] = False
     return primes
 
+def generate_primes_v3(n: int) -> List[int]:
+    '''
+    Books much more optimized sieve version
+    '''
+    if n < 2:
+        return []
+    primes = [2]
+    sz = ((n - 3) // 2) + 1
+    # For each i in is_prime the prime number it represents is 2 * i + 3
+    is_prime = [True] * sz
+    for i in range(sz):
+        if is_prime[i]:
+            p = 2 * i + 3
+            primes.append(p)
+            for j in range(2 * i**2 + 6 * i + 3, sz, p):
+                is_prime[j] = False
+    return primes
+
+
+
 # Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
-    return generate_primes_v2(n)
+    # return generate_primes_v2(n)
+    return generate_primes_v3(n)
 
 
 if __name__ == '__main__':
     TestEnumeratePrimes(generate_primes_v1).run_tests()
     TestEnumeratePrimes(generate_primes_v2).run_tests()
+    TestEnumeratePrimes(generate_primes_v3).run_tests()
     exit(
         generic_test.generic_test_main('prime_sieve.py', 'prime_sieve.tsv',
                                        generate_primes))
