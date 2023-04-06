71ef7acbe955ca6df841e5d151ef469c1ab7e140
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-04-06 13:20:07 +0530

    A1-python: Preparing for look and say series

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -1,5 +1,12 @@
 from test_framework import generic_test
+from tests.test_look_and_say import TestLookAndSay
 
+
+def look_and_say_v1(n: int) -> str:
+    '''
+    My version
+    '''
+    pass
 
 def look_and_say(n: int) -> str:
     # TODO - you fill in here.
@@ -7,6 +14,7 @@ def look_and_say(n: int) -> str:
 
 
 if __name__ == '__main__':
+    TestLookAndSay(look_and_say_v1).run_tests()
     exit(
         generic_test.generic_test_main('look_and_say.py', 'look_and_say.tsv',
                                        look_and_say))
---
+++ epi_judge_python/tests/test_look_and_say.py
@@ -0,0 +1,8 @@
+from tests.test_base import TestBase
+
+class TestLookAndSay(TestBase):
+    look_and_say_series = ['1', '11', '21', '1211', '111221', '312211', '13112221', '1113213211', '31131211131221']
+
+    def test_example1(self):
+        for i, result in enumerate(TestLookAndSay.look_and_say_series):
+            assert self.solve(i + 1) == result
