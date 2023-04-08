6b121a28316ccebdf80302226d3d90d42e008ef1
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-04-08 22:41:31 +0530

    A1-python: Starting with linked list

---
+++ epi_judge_python/tests/test_search_in_list.py
@@ -0,0 +1,7 @@
+from tests.test_base import TestBase
+
+class TestSearchInList(TestBase):
+    def test_example(self):
+        # How to test linked list here
+        # I will have to check how it is done in the framework
+        pass
