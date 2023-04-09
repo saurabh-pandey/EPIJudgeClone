19daca8137988b3c1d4a8e3e051ed699aaa95650
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-04-09 17:08:31 +0530

    A1-python: Implemented searching in linked list

--- epi_judge_python/search_in_list.py
+++ epi_judge_python/search_in_list.py
@@ -1,10 +1,21 @@
 from list_node import ListNode
 from test_framework import generic_test
+from tests.test_search_in_list import TestSearchInList
 
+
+def search_list_v1(L: ListNode, key: int) -> ListNode:
+    '''
+    My O(n) version
+    '''
+    curr_node = L
+    while curr_node:
+        if curr_node.data == key:
+            return curr_node
+        curr_node = curr_node.next
+    return None
 
 def search_list(L: ListNode, key: int) -> ListNode:
-    # TODO - you fill in here.
-    return ListNode()
+    return search_list_v1(L, key)
 
 
 def search_list_wrapper(L, key):
@@ -13,6 +24,7 @@ def search_list_wrapper(L, key):
 
 
 if __name__ == '__main__':
+    TestSearchInList(search_list_v1).run_tests()
     exit(
         generic_test.generic_test_main('search_in_list.py',
                                        'search_in_list.tsv',
--- epi_judge_python/tests/test_search_in_list.py
+++ epi_judge_python/tests/test_search_in_list.py
@@ -1,9 +1,40 @@
 from tests.test_base import TestBase
 
-from list_node import ListNode
+from tests.utils import linked_list
+
 
 class TestSearchInList(TestBase):
-    def test_example(self):
-        # How to test linked list here
-        # I will have to check how it is done in the framework
-        pass
+    def test_example1(self):
+        l = linked_list.create([1, 2, 3])
+        node = self.solve(l, 2)
+        assert node.data == 2
+    
+    def test_example2(self):
+        l = linked_list.create([1, 2, 3])
+        node = self.solve(l, 1)
+        assert node.data == 1
+    
+    def test_example3(self):
+        l = linked_list.create([1, 2, 3])
+        node = self.solve(l, 3)
+        assert node.data == 3
+    
+    def test_example4(self):
+        l = linked_list.create([1, 2, 3])
+        node = self.solve(l, 4)
+        assert node == None
+    
+    def test_example5(self):
+        l = linked_list.create([])
+        node = self.solve(l, 4)
+        assert node == None
+    
+    def test_example6(self):
+        l = linked_list.create([1])
+        node = self.solve(l, 1)
+        assert node.data == 1
+    
+    def test_example7(self):
+        l = linked_list.create([1])
+        node = self.solve(l, 2)
+        assert node == None
