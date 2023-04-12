4949882bb2c47b819cc5ca31d4ed06e9cab763e5
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-04-12 16:27:33 +0530

    A1-python: Solved reversing a sublist of a linked list using recursive reversal function. One test fails due to stack overflow

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Tuple
 
 from list_node import ListNode
 from test_framework import generic_test
@@ -9,18 +9,46 @@ def reverse_sublist_v1(L: ListNode,
                        start: int,
                        finish: int) -> Optional[ListNode]:
     '''
-    My version
+    My version with O(n) time and space
     '''
-    pass
+    def reverse(node: ListNode, depth: int) -> Tuple[ListNode, ListNode]:
+        if not node.next or depth == 1:
+            return node, node
+        new_head, _ = reverse(node.next, depth - 1)
+        node.next.next = node
+        return new_head, node
+    if not L:
+        return L
+    if start == finish:
+        return L
+    count = 1
+    prev_to_start = None
+    start_node = L
+    while count < start:
+        count += 1
+        prev_to_start = start_node
+        start_node = start_node.next
+    finish_node = start_node
+    while count <= finish:
+        count += 1
+        finish_node = finish_node.next
+    rev_head, rev_tail = reverse(start_node, finish - start + 1)
+    rev_tail.next = finish_node
+    if prev_to_start:
+        prev_to_start.next = rev_head
+        return L
+    else:
+        return rev_head
+
 
 def reverse_sublist(L: ListNode, start: int,
                     finish: int) -> Optional[ListNode]:
     # TODO - you fill in here.
-    return None
+    return reverse_sublist_v1(L, start, finish)
 
 
 if __name__ == '__main__':
     TestReverseSublist(reverse_sublist_v1).run_tests()
-    # exit(
-    #     generic_test.generic_test_main('reverse_sublist.py',
-    #                                    'reverse_sublist.tsv', reverse_sublist))
+    exit(
+        generic_test.generic_test_main('reverse_sublist.py',
+                                       'reverse_sublist.tsv', reverse_sublist))
--- epi_judge_python/tests/test_reverse_sublist.py
+++ epi_judge_python/tests/test_reverse_sublist.py
@@ -1,6 +1,7 @@
 from tests.test_base import TestBase
 from tests.utils import linked_list
 
+
 class TestReverseSublist(TestBase):
     def test_example1(self):
         l = linked_list.create([11, 3, 5, 7, 2])
@@ -24,4 +25,28 @@ class TestReverseSublist(TestBase):
         finish = 3
         l = self.solve(l, start, finish)
         expected = linked_list.create([11, 7, 5, 3, 2])
+        assert l == expected
+    
+    def test_example4(self):
+        l = linked_list.create([2])
+        start = 1
+        finish = 1
+        l = self.solve(l, start, finish)
+        expected = linked_list.create([2])
+        assert l == expected
+    
+    def test_empty(self):
+        l = linked_list.create([])
+        start = 0
+        finish = 0
+        l = self.solve(l, start, finish)
+        expected = linked_list.create([])
+        assert l == expected
+    
+    def test_single(self):
+        l = linked_list.create([1])
+        start = 0
+        finish = 0
+        l = self.solve(l, start, finish)
+        expected = linked_list.create([1])
         assert l == expected
