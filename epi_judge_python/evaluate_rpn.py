c22dae64bcf42243df6c6537c89b5e321bc7ee62
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-04-18 18:39:49 +0530

    A1-python: Implemented an RPN evaluator

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -7,15 +7,39 @@ def evaluate_v1(expression: str) -> int:
     '''
     My O(n) space and O(n) time version where n is length of string
     '''
-    pass
+    def eval(s, stack):
+        if s in ("+", "-", "*", "/"):
+            num2 = stack.pop()
+            num1 = stack.pop()
+            if s == "+":
+                stack.append(num1 + num2)
+            elif s == "-":
+                stack.append(num1 - num2)
+            elif s == "*":
+                stack.append(num1 * num2)
+            elif s == "/":
+                assert num2 != 0
+                stack.append(num1 // num2)
+        else:
+            stack.append(int(s))
+    eval_stack = []
+    sub_str = ""
+    for s in expression:
+        if s == ",":
+            eval(sub_str, eval_stack)
+            sub_str = ""
+        else:
+            sub_str += s
+    eval(sub_str, eval_stack)
+    return eval_stack[0]
+
 
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    return evaluate_v1(expression)
 
 
 if __name__ == '__main__':
     TestEvaluateRPN(evaluate_v1).run_tests()
-    # exit(
-    #     generic_test.generic_test_main('evaluate_rpn.py', 'evaluate_rpn.tsv',
-    #                                    evaluate))
+    exit(
+        generic_test.generic_test_main('evaluate_rpn.py', 'evaluate_rpn.tsv',
+                                       evaluate))
--- epi_judge_python/tests/test_evaluate_rpn.py
+++ epi_judge_python/tests/test_evaluate_rpn.py
@@ -1,5 +1,7 @@
 from tests.test_base import TestBase
 
+import pdb
+
 class TestEvaluateRPN(TestBase):
     def test_example1(self):
         rpn = "3,4,+,2,*,1,+"
@@ -11,10 +13,12 @@ class TestEvaluateRPN(TestBase):
     
     def test_example3(self):
         rpn = "5"
+        # pdb.set_trace()
         assert self.solve(rpn) == 5
     
     def test_example4(self):
         rpn = "-6"
+        # pdb.set_trace()
         assert self.solve(rpn) == -6
     
     def test_example5(self):
@@ -23,4 +27,14 @@ class TestEvaluateRPN(TestBase):
     
     def test_example6(self):
         rpn = "-6,-4,+,2,*,1,-,7,/,2,/"
-        assert self.solve(rpn) == -1
+        # pdb.set_trace()
+        res = self.solve(rpn)
+        # print(f"Res = {res}")
+        expected = -2
+        assert res == expected, f"expected = {expected}, actual = {res}"
+    
+    def test_attempt1(self):
+        rpn = "11,8,10,*,+,7,12,*,+,12,7,*,+,9,14,*,+,17,+"
+        res = self.solve(rpn)
+        expected = 402
+        assert res == expected, f"expected = {expected}, actual = {res}"
