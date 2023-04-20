d79fc55ae3815ab7ad77b549da72edbd0c748584
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-04-20 21:05:40 +0530

    A1-python: Solved level order traversal of a tree

--- epi_judge_python/tests/test_tree_level_order.py
+++ epi_judge_python/tests/test_tree_level_order.py
@@ -1,5 +1,30 @@
 from tests.test_base import TestBase
 
+from binary_tree_node import BinaryTreeNode as N
+
 class TestTreeLevelOrder(TestBase):
     def test_example1(self):
-        
+        tree = N(1,
+                 N(2, N(4, None, N(7)), N(5)),
+                 N(3, None, N(6, N(9))))
+        expected = [[1], [2, 3], [4, 5, 6], [7, 9]]
+        actual = self.solve(tree)
+        assert expected == actual, f"expected = {expected}, actual = {actual}"
+    
+    def test_left_sided(self):
+        tree = N(1,N(2, N(3, N(4))))
+        expected = [[1], [2], [3], [4]]
+        actual = self.solve(tree)
+        assert expected == actual, f"expected = {expected}, actual = {actual}"
+
+    def test_right_sided(self):
+        tree = N(1, None, N(2, None, N(3, None, N(4))))
+        expected = [[1], [2], [3], [4]]
+        actual = self.solve(tree)
+        assert expected == actual, f"expected = {expected}, actual = {actual}"
+    
+    def test_error1(self):
+        tree = None
+        expected = []
+        actual = self.solve(tree)
+        assert expected == actual, f"expected = {expected}, actual = {actual}"
--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -3,13 +3,34 @@ from typing import List
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+from tests.test_tree_level_order import TestTreeLevelOrder
+
+
+def binary_tree_depth_order_v1(tree: BinaryTreeNode) -> List[List[int]]:
+    '''
+    My version
+    '''
+    if not tree:
+        return []
+    result = []
+    nodes_queue = [tree]
+    while nodes_queue:
+        result.append([node.data for node in nodes_queue])
+        next_nodes = []
+        for node in nodes_queue:
+            if node.left:
+                next_nodes.append(node.left)
+            if node.right:
+                next_nodes.append(node.right)
+        nodes_queue = next_nodes
+    return result
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    return binary_tree_depth_order_v1(tree)
 
 
 if __name__ == '__main__':
+    TestTreeLevelOrder(binary_tree_depth_order_v1).run_tests()
     exit(
         generic_test.generic_test_main('tree_level_order.py',
                                        'tree_level_order.tsv',
