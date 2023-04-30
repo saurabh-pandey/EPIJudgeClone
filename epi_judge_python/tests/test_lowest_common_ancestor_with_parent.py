add52225f7d7d54d8735f5604f051892c869dcda
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-04-30 16:58:58 +0530

    A1-python: Level order serialization and deserialization of a binary tree

--- epi_judge_python/tests/test_lowest_common_ancestor_with_parent.py
+++ epi_judge_python/tests/test_lowest_common_ancestor_with_parent.py
@@ -3,14 +3,20 @@ from tests.utils import binary_tree
 
 from binary_tree_with_parent_prototype import BinaryTreeNode as N
 
+import pdb
+
 class TestLowestCommonAncestorWithParent(TestBase):
     def test_example1(self):
-        nodes = [N(i) for i in range(1, 8)]
-        nodes[0].left 
-        tree = nodes[0]
-        node0 = binary_tree.find_node(tree, 7)
-        node1 = binary_tree.find_node(tree, 5)
-        expected = binary_tree.find_node(tree, 2)
-        result = self.solve(tree, node0, node1)
-        assert result is expected, (
-            f"expected = {expected.data}, result = {result.data}")
+        nodes = [1, 2, 3, 4, 5, None, 6, None, 7, 8, 9, None, None, None, None, None, None, 10]
+        # pdb.set_trace()
+        print(f"Original nodes = {nodes}")
+        tree = binary_tree.deserialize_level_order(nodes)
+        # pdb.set_trace()
+        new_nodes = binary_tree.serialize_level_order(tree)
+        print(f"Computed nodes = {new_nodes}")
+        # node0 = binary_tree.find_node(tree, 7)
+        # node1 = binary_tree.find_node(tree, 5)
+        # expected = binary_tree.find_node(tree, 2)
+        # result = self.solve(tree, node0, node1)
+        # assert result is expected, (
+        #     f"expected = {expected.data}, result = {result.data}")
--- epi_judge_python/tests/utils/binary_tree.py
+++ epi_judge_python/tests/utils/binary_tree.py
@@ -1,6 +1,46 @@
-from typing import Optional
+from typing import List, Optional
 
 from binary_tree_node import BinaryTreeNode
+
+def deserialize_level_order(
+        nodes: List[Optional[int]]) -> Optional[BinaryTreeNode]:
+    if not nodes:
+        return None
+    cloned_nodes = nodes[:]
+    root_key = None
+    while root_key is None:
+        root_key = cloned_nodes.pop(0)
+    root = BinaryTreeNode(root_key)
+    nodes_queue = [root]
+    while cloned_nodes:
+        parent_node = nodes_queue.pop(0)
+        if parent_node:
+            left_child_key = cloned_nodes.pop(0)
+            right_child_key = cloned_nodes.pop(0) if cloned_nodes else None
+            if left_child_key:
+                parent_node.left = BinaryTreeNode(left_child_key)
+                nodes_queue.append(parent_node.left)
+            if right_child_key:
+                parent_node.right = BinaryTreeNode(right_child_key)
+                nodes_queue.append(parent_node.right)
+    return root
+
+def serialize_level_order(
+        tree: Optional[BinaryTreeNode]) -> List[Optional[int]]:
+    if tree is None:
+        return []
+    nodes_queue = [tree]
+    serialized_tree = [tree.data]
+    while nodes_queue:
+        node = nodes_queue.pop(0)
+        if node:
+            serialized_tree.append(node.left.data if node.left else None)
+            serialized_tree.append(node.right.data if node.right else None)
+            nodes_queue.append(node.left)
+            nodes_queue.append(node.right)
+    while serialized_tree[-1] is None:
+        serialized_tree.pop()
+    return serialized_tree
 
 
 def find_node(node: BinaryTreeNode, key: int) -> Optional[BinaryTreeNode]:
