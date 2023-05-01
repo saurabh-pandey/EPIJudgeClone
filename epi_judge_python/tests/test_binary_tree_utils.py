a2f801f37ce125640a8ffb2f5b8dfeb3ada1f429
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-01 13:26:14 +0530

    A1-python: Added new test ideas for my binary tree utils.

--- epi_judge_python/tests/test_binary_tree_utils.py
+++ epi_judge_python/tests/test_binary_tree_utils.py
@@ -1,23 +1,21 @@
 from tests.test_base import TestBase
 from tests.utils import binary_tree
 
-import pdb
+from binary_tree_node import BinaryTreeNode as N
+
 
 class TestBinaryTreeUtils(TestBase):
+    def test_tree1(self):
+        tree = N(1, N(2, N(4), N(5, N(8), None)),
+             N(3, N(6, None, N(9)), N(7, None, N(10, N(11), N(12)))))
+        # Test all functionalities on this tree
+    
     def test_level_order(self):
         nodes = [1, 2, 3, 4, 5, None, 6, None, 7, 8, 9, None, None, None, None, None, None, 10]
-        # pdb.set_trace()
         print(f"Original nodes = {nodes}")
         tree = binary_tree.LevelOrder.deserialize(nodes)
-        # pdb.set_trace()
         new_nodes = binary_tree.LevelOrder.serialize(tree)
         print(f"Computed nodes = {new_nodes}")
-        # node0 = binary_tree.find_node(tree, 7)
-        # node1 = binary_tree.find_node(tree, 5)
-        # expected = binary_tree.find_node(tree, 2)
-        # result = self.solve(tree, node0, node1)
-        # assert result is expected, (
-        #     f"expected = {expected.data}, result = {result.data}")
         nodes = [1, 2, 3]
         print(f"Original nodes = {nodes}")
         tree = binary_tree.LevelOrder.deserialize(nodes)
@@ -26,18 +24,10 @@ class TestBinaryTreeUtils(TestBase):
     
     def test_pre_order(self):
         nodes = [1, 2, None, 4, None, None, 3]
-        # pdb.set_trace()
         print(f"Original nodes = {nodes}")
         tree = binary_tree.PreOrder.deserialize(nodes)
-        # pdb.set_trace()
         new_nodes = binary_tree.PreOrder.serialize(tree)
         print(f"Computed nodes = {new_nodes}")
-        # node0 = binary_tree.find_node(tree, 7)
-        # node1 = binary_tree.find_node(tree, 5)
-        # expected = binary_tree.find_node(tree, 2)
-        # result = self.solve(tree, node0, node1)
-        # assert result is expected, (
-        #     f"expected = {expected.data}, result = {result.data}")
         nodes = [1, 2, 3]
         print(f"Original nodes = {nodes}")
         tree = binary_tree.PreOrder.deserialize(nodes)
