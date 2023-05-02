bec3f681b40dbe4dd2a433302828f9b189f9f332
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-02 14:19:45 +0530

    A1-python: Added one more test to check all binary tree utils functions.

--- epi_judge_python/tests/test_binary_tree_utils.py
+++ epi_judge_python/tests/test_binary_tree_utils.py
@@ -6,9 +6,41 @@ from binary_tree_node import BinaryTreeNode as N
 
 class TestBinaryTreeUtils(TestBase):
     def test_tree1(self):
-        tree = N(1, N(2, N(4), N(5, N(8), None)),
-             N(3, N(6, None, N(9)), N(7, None, N(10, N(11), N(12)))))
-        # Test all functionalities on this tree
+        tree = N(1,
+                 N(2,
+                   N(4),
+                   N(5,
+                     N(8),
+                     None
+                    )
+                 ),
+                 N(3,
+                   N(6,
+                     None,
+                     N(9)
+                    ),
+                   N(7,
+                     None,
+                     N(10,
+                       N(11),
+                       N(12)
+                      )
+                    )
+                 )
+                )
+        lo_serial_tree = binary_tree.LevelOrder.serialize(tree)
+        lo_deserial_tree = binary_tree.LevelOrder.deserialize(lo_serial_tree)
+        lo_reserial_tree = binary_tree.LevelOrder.serialize(lo_deserial_tree)
+        assert lo_reserial_tree == lo_serial_tree
+        po_serial_tree = binary_tree.PreOrder.serialize(tree)
+        po_deserial_tree = binary_tree.PreOrder.deserialize(po_serial_tree)
+        po_reserial_tree = binary_tree.PreOrder.serialize(po_deserial_tree)
+        assert po_reserial_tree == po_serial_tree
+        assert binary_tree.find_node(tree, 0) == None
+        for i in range(1, 13):
+            assert binary_tree.find_node(tree, i).data == i
+        for i in range(13, 21):
+            assert binary_tree.find_node(tree, i) == None
     
     def test_level_order(self):
         nodes = [1, 2, 3, 4, 5, None, 6, None, 7, 8, 9, None, None, None, None, None, None, 10]
