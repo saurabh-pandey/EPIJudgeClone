e9406be314d696b6935958ad31c85c2100430087
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-04 13:09:37 +0530

    A1-python: Adding tests to check binary tree with parent node

--- epi_judge_python/tests/test_binary_tree_utils.py
+++ epi_judge_python/tests/test_binary_tree_utils.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from tests.test_base import TestBase
-from tests.utils import binary_tree
+from tests.utils import binary_tree as bt
 
 from binary_tree_node import BinaryTreeNode as N
 from binary_tree_with_parent_prototype import BinaryTreeNode as NodeWithParent
@@ -9,81 +9,71 @@ from binary_tree_with_parent_prototype import BinaryTreeNode as NodeWithParent
 
 class TestBinaryTreeUtils(TestBase):
     def _check_tree(self, tree: Optional[N], max_key: Optional[int]):
-        pass
+        lo_serial_tree = bt.LevelOrder.serialize(tree)
+        lo_deserial_tree = bt.LevelOrder.deserialize(lo_serial_tree)
+        lo_reserial_tree = bt.LevelOrder.serialize(lo_deserial_tree)
+        assert lo_reserial_tree == lo_serial_tree
+        po_serial_tree = bt.PreOrder.serialize(tree)
+        po_deserial_tree = bt.PreOrder.deserialize(po_serial_tree)
+        po_reserial_tree = bt.PreOrder.serialize(po_deserial_tree)
+        assert po_reserial_tree == po_serial_tree
+        if max_key is not None:
+            assert bt.find_node(tree, 0) == None
+            for i in range(1, max_key + 1):
+                assert bt.find_node(tree, i).data == i
+            for i in range(max_key + 1, max_key + 20):
+                assert bt.find_node(tree, i) == None
+        else:
+            for i in range(0, 20):
+                assert bt.find_node(tree, i) == None
+    
+    def _check_tree_with_parent(self, tree: Optional[N],
+                                max_key: Optional[int]):
+        lo_serial_tree = bt.LevelOrder.serialize(tree)
+        lo_deserial_tree = bt.LevelOrder.deserialize(lo_serial_tree,
+                                                     NodeWithParent)
+        if lo_deserial_tree is not None and lo_deserial_tree.left is not None:
+            assert lo_deserial_tree.left.parent is lo_deserial_tree
+        if lo_deserial_tree is not None and lo_deserial_tree.right is not None:
+            assert lo_deserial_tree.right.parent is lo_deserial_tree
+        lo_reserial_tree = bt.LevelOrder.serialize(lo_deserial_tree)
+        assert lo_reserial_tree == lo_serial_tree
+        po_serial_tree = bt.PreOrder.serialize(tree)
+        po_deserial_tree = bt.PreOrder.deserialize(po_serial_tree, 
+                                                   NodeWithParent)
+        po_reserial_tree = bt.PreOrder.serialize(po_deserial_tree)
+        assert po_reserial_tree == po_serial_tree
+        if max_key is not None:
+            assert bt.find_node(tree, 0) == None
+            for i in range(1, max_key + 1):
+                assert bt.find_node(tree, i).data == i
+            for i in range(max_key + 1, max_key + 20):
+                assert bt.find_node(tree, i) == None
+        else:
+            for i in range(0, 20):
+                assert bt.find_node(tree, i) == None
     
     def test_tree1(self):
-        tree = binary_tree.BinaryTreeFactory.tree1()
-        lo_serial_tree = binary_tree.LevelOrder.serialize(tree)
-        lo_deserial_tree = binary_tree.LevelOrder.deserialize(lo_serial_tree)
-        lo_reserial_tree = binary_tree.LevelOrder.serialize(lo_deserial_tree)
-        assert lo_reserial_tree == lo_serial_tree
-        po_serial_tree = binary_tree.PreOrder.serialize(tree)
-        po_deserial_tree = binary_tree.PreOrder.deserialize(po_serial_tree)
-        po_reserial_tree = binary_tree.PreOrder.serialize(po_deserial_tree)
-        assert po_reserial_tree == po_serial_tree
-        assert binary_tree.find_node(tree, 0) == None
-        for i in range(1, 13):
-            assert binary_tree.find_node(tree, i).data == i
-        for i in range(13, 21):
-            assert binary_tree.find_node(tree, i) == None
+        tree = bt.BinaryTreeFactory.tree1()
+        self._check_tree(tree, 12)
+        self._check_tree_with_parent(tree, 12)
     
     def test_tree2(self):
-        tree = binary_tree.BinaryTreeFactory.tree2()
-        lo_serial_tree = binary_tree.LevelOrder.serialize(tree)
-        lo_deserial_tree = binary_tree.LevelOrder.deserialize(lo_serial_tree)
-        lo_reserial_tree = binary_tree.LevelOrder.serialize(lo_deserial_tree)
-        assert lo_reserial_tree == lo_serial_tree
-        po_serial_tree = binary_tree.PreOrder.serialize(tree)
-        po_deserial_tree = binary_tree.PreOrder.deserialize(po_serial_tree)
-        po_reserial_tree = binary_tree.PreOrder.serialize(po_deserial_tree)
-        assert po_reserial_tree == po_serial_tree
-        assert binary_tree.find_node(tree, 0) == None
-        for i in range(1, 11):
-            assert binary_tree.find_node(tree, i).data == i
-        for i in range(11, 21):
-            assert binary_tree.find_node(tree, i) == None
+        tree = bt.BinaryTreeFactory.tree2()
+        self._check_tree(tree, 10)
+        self._check_tree_with_parent(tree, 10)
     
     def test_single(self):
-        tree = binary_tree.BinaryTreeFactory.single_node()
-        lo_serial_tree = binary_tree.LevelOrder.serialize(tree)
-        lo_deserial_tree = binary_tree.LevelOrder.deserialize(lo_serial_tree)
-        lo_reserial_tree = binary_tree.LevelOrder.serialize(lo_deserial_tree)
-        assert lo_reserial_tree == lo_serial_tree
-        po_serial_tree = binary_tree.PreOrder.serialize(tree)
-        po_deserial_tree = binary_tree.PreOrder.deserialize(po_serial_tree)
-        po_reserial_tree = binary_tree.PreOrder.serialize(po_deserial_tree)
-        assert po_reserial_tree == po_serial_tree
-        assert binary_tree.find_node(tree, 0) == None
-        for i in range(1, 2):
-            assert binary_tree.find_node(tree, i).data == i
-        for i in range(2, 10):
-            assert binary_tree.find_node(tree, i) == None
+        tree = bt.BinaryTreeFactory.single_node()
+        self._check_tree(tree, 1)
+        self._check_tree_with_parent(tree, 1)
     
     def test_tree3(self):
-        tree = binary_tree.BinaryTreeFactory.tree3()
-        lo_serial_tree = binary_tree.LevelOrder.serialize(tree)
-        lo_deserial_tree = binary_tree.LevelOrder.deserialize(lo_serial_tree)
-        lo_reserial_tree = binary_tree.LevelOrder.serialize(lo_deserial_tree)
-        assert lo_reserial_tree == lo_serial_tree
-        po_serial_tree = binary_tree.PreOrder.serialize(tree)
-        po_deserial_tree = binary_tree.PreOrder.deserialize(po_serial_tree)
-        po_reserial_tree = binary_tree.PreOrder.serialize(po_deserial_tree)
-        assert po_reserial_tree == po_serial_tree
-        assert binary_tree.find_node(tree, 0) == None
-        for i in range(1, 6):
-            assert binary_tree.find_node(tree, i).data == i
-        for i in range(6, 10):
-            assert binary_tree.find_node(tree, i) == None
+        tree = bt.BinaryTreeFactory.tree3()
+        self._check_tree(tree, 5)
+        self._check_tree_with_parent(tree, 5)
     
     def test_empty(self):
         tree = None
-        lo_serial_tree = binary_tree.LevelOrder.serialize(tree)
-        lo_deserial_tree = binary_tree.LevelOrder.deserialize(lo_serial_tree)
-        lo_reserial_tree = binary_tree.LevelOrder.serialize(lo_deserial_tree)
-        assert lo_reserial_tree == lo_serial_tree
-        po_serial_tree = binary_tree.PreOrder.serialize(tree)
-        po_deserial_tree = binary_tree.PreOrder.deserialize(po_serial_tree)
-        po_reserial_tree = binary_tree.PreOrder.serialize(po_deserial_tree)
-        assert po_reserial_tree == po_serial_tree
-        for i in range(0, 10):
-            assert binary_tree.find_node(tree, i) == None
+        self._check_tree(tree, None)
+        self._check_tree_with_parent(tree, None)
