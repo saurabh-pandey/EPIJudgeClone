e807176f708f8f89ae5f75e7f1827ba2a0ffd6dd
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-09 11:21:05 +0530

    A1-python: Done with constructing binary tree from preorder and inorder lists

--- epi_judge_python/tree_from_preorder_inorder.py
+++ epi_judge_python/tree_from_preorder_inorder.py
@@ -10,10 +10,11 @@ def binary_tree_from_preorder_inorder_v1(
         inorder: List[int]
         ) -> Optional[BinaryTreeNode]:
     '''
-    My O(n) version
+    My O(n^2) version
     '''
     if not preorder:
         return None
+    # This one takes O(n) time
     partition_index = inorder.index(preorder[0])
     root = BinaryTreeNode(preorder[0])
     root.left = binary_tree_from_preorder_inorder_v1(
@@ -27,14 +28,46 @@ def binary_tree_from_preorder_inorder_v1(
     return root
 
 
+def binary_tree_from_preorder_inorder_v2(
+        preorder: List[int],
+        inorder: List[int]
+        ) -> Optional[BinaryTreeNode]:
+    '''
+    Book's O(n) version
+    '''
+    node_to_index = {data: i for i, data in enumerate(inorder)}
+    def construct_tree(preorder_start: int,
+                       preorder_end: int,
+                       inorder_start: int,
+                       inorder_end: int) -> Optional[BinaryTreeNode]:
+        if preorder_end <= preorder_start or inorder_end <= inorder_start:
+            return None
+        root = BinaryTreeNode(preorder[preorder_start])
+        partition_index = node_to_index[preorder[preorder_start]]
+        left_subtree_sz = partition_index - inorder_start
+        root.left = construct_tree(preorder_start + 1,
+                                   preorder_start + left_subtree_sz + 1,
+                                   inorder_start,
+                                   partition_index)
+        root.right = construct_tree(preorder_start + left_subtree_sz + 1,
+                                    preorder_end,
+                                    partition_index + 1,
+                                    inorder_end)
+        return root
+    return construct_tree(0, len(preorder), 0, len(inorder))
+
+
 def binary_tree_from_preorder_inorder(preorder: List[int],
                                       inorder: List[int]) -> BinaryTreeNode:
-    return binary_tree_from_preorder_inorder_v1(preorder, inorder)
+    # return binary_tree_from_preorder_inorder_v1(preorder, inorder)
+    return binary_tree_from_preorder_inorder_v2(preorder, inorder)
 
 
 if __name__ == '__main__':
     TestTreeFromPreorderInorder(
         binary_tree_from_preorder_inorder_v1).run_tests()
+    TestTreeFromPreorderInorder(
+        binary_tree_from_preorder_inorder_v2).run_tests()
     exit(
         generic_test.generic_test_main('tree_from_preorder_inorder.py',
                                        'tree_from_preorder_inorder.tsv',
