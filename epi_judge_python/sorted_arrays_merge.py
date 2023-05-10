28bcb6ed97312bf921856735339ff9c054e1eb14
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-10 15:43:28 +0530

    A1-python: Merge multiple sorted arrays into one sorted array

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,7 +1,32 @@
+import sys
 from typing import List
 
 from test_framework import generic_test
+from tests.test_sorted_arrays_merge import TestSortedArraysMerge
 
+
+def merge_sorted_arrays_v1(sorted_arrays: List[List[int]]) -> List[int]:
+    '''
+    Simple O(nk) version
+    '''
+    total_len = sum([len(arr) for arr in sorted_arrays])
+    result = [0 for _ in range(total_len)]
+    running_arr_indices = [0 for _ in sorted_arrays]
+    index = 0
+    while index < total_len:
+        min_val = sys.maxsize
+        arr_index = 0
+        for i, arr in enumerate(sorted_arrays):
+            curr_arr_index = running_arr_indices[i]
+            if curr_arr_index < len(arr):
+                curr_value = arr[curr_arr_index]
+                if curr_value < min_val:
+                    min_val = curr_value
+                    arr_index = i
+        result[index] = min_val
+        index += 1
+        running_arr_indices[arr_index] += 1
+    return result
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
     # TODO - you fill in here.
@@ -9,7 +34,8 @@ def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('sorted_arrays_merge.py',
-                                       'sorted_arrays_merge.tsv',
-                                       merge_sorted_arrays))
+    TestSortedArraysMerge(merge_sorted_arrays_v1).run_tests()
+    # exit(
+    #     generic_test.generic_test_main('sorted_arrays_merge.py',
+    #                                    'sorted_arrays_merge.tsv',
+    #                                    merge_sorted_arrays))
---
+++ epi_judge_python/tests/test_sorted_arrays_merge.py
@@ -0,0 +1,34 @@
+import random
+
+from tests.test_base import TestBase
+
+
+class TestSortedArraysMerge(TestBase):
+    def test_example1(self):
+        sorted_arrays = [[3,5,7], [0, 6], [0, 6, 28]]
+        result = self.solve(sorted_arrays)
+        assert all(result[i] <= result[i + 1] for i in range(len(result) - 1))
+    
+    def test_random(self):
+        for min_size in range(1, 21):
+            for inc in range(21):
+                max_size = min_size + inc
+                for k in range(1, 50):
+                    # print(f"Running for {min_size}, {max_size}, {k}")
+                    sorted_arrays = self._generate_sorted_arrays(min_size, 
+                                                                 max_size ,
+                                                                 k)
+                    result = self.solve(sorted_arrays)
+                    assert all(
+                        result[i] <= result[i + 1] for i in range(
+                        len(result) - 1)), (
+                        f"Failed for {min_size}, {max_size}, {k}")
+    
+    def _generate_sorted_arrays(self, min_size, max_size, k):
+        sorted_arrays = []
+        for _ in range(k):
+            size = random.randint(min_size, max_size)
+            arr = [random.randint(0, 2 * size) for _ in range(size)]
+            arr.sort()
+            sorted_arrays.append(arr)
+        return sorted_arrays
