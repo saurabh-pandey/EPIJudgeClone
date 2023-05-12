87ff2bbac4659f84d0237f4e49feacf05e943b9c
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-12 12:53:04 +0530

    A1-python: Done with merging sorted arrays

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -39,26 +39,49 @@ def merge_sorted_arrays_v2(sorted_arrays: List[List[int]]) -> List[int]:
     total_len = sum([len(arr) for arr in sorted_arrays])
     result = [0] * total_len
     index = 0
-    h = [(arr[0], 0, i) for i, arr in enumerate(sorted_arrays)]
-    heapq.heapify(h)
+    min_heap = [(arr[0], 0, i) for i, arr in enumerate(sorted_arrays)]
+    heapq.heapify(min_heap)
     while index < total_len:
-        min_val, i, arr_i = heapq.heappop(h)
+        min_val, i, arr_i = heapq.heappop(min_heap)
         result[index] = min_val
         index += 1
         sorted_arr = sorted_arrays[arr_i]
         if i + 1 < len(sorted_arr):
-            heapq.heappush(h, (sorted_arr[i + 1], i + 1, arr_i))
+            heapq.heappush(min_heap, (sorted_arr[i + 1], i + 1, arr_i))
+    return result
+
+
+def merge_sorted_arrays_v3(sorted_arrays: List[List[int]]) -> List[int]:
+    '''
+    Book's O(nlog(k)) version using iterators
+    '''
+    sorted_arr_iters = [iter(arr) for arr in sorted_arrays]
+    min_heap = []
+    for i, it in enumerate(sorted_arr_iters):
+        first_value = next(it, None)
+        if first_value is not None:
+            heapq.heappush(min_heap, (first_value, i))
+    result = []
+    while min_heap:
+        value, i = heapq.heappop(min_heap)
+        result.append(value)
+        sorted_arr_it = sorted_arr_iters[i]
+        next_val = next(sorted_arr_it, None)
+        if next_val is not None:
+            heapq.heappush(min_heap, (next_val, i))
     return result
 
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
     # return merge_sorted_arrays_v1(sorted_arrays)
-    return merge_sorted_arrays_v2(sorted_arrays)
+    # return merge_sorted_arrays_v2(sorted_arrays)
+    return merge_sorted_arrays_v3(sorted_arrays)
 
 
 if __name__ == '__main__':
     TestSortedArraysMerge(merge_sorted_arrays_v1).run_tests()
     TestSortedArraysMerge(merge_sorted_arrays_v2).run_tests()
+    TestSortedArraysMerge(merge_sorted_arrays_v3).run_tests()
     exit(
         generic_test.generic_test_main('sorted_arrays_merge.py',
                                        'sorted_arrays_merge.tsv',
