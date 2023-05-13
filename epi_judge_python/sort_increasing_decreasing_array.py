602eac87f5f999d9237192b66d33c348ec332be9
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-13 14:44:52 +0530

    A1-python: Solved sorting increasing decreasing array or k-sorted array

--- epi_judge_python/sort_increasing_decreasing_array.py
+++ epi_judge_python/sort_increasing_decreasing_array.py
@@ -1,3 +1,5 @@
+import heapq
+
 from typing import List
 
 from test_framework import generic_test
@@ -7,13 +9,65 @@ from tests.test_sort_increasing_decreasing_array import (
 
 def sort_k_increasing_decreasing_array_v1(A: List[int]) -> List[int]:
     '''
-    My version
+    My version with O(nlog(k)) runtime
     '''
-    return []
+    if not A:
+        return []
+    if len(A) == 1:
+        return A[:]
+    array_bounds = {
+        "inc": [],
+        "dec": []
+    }
+    is_increasing = A[0] <= A[1]
+    if is_increasing:
+        array_bounds["inc"].append(0)
+    else:
+        array_bounds["dec"].append(0)
+    for i in range(len(A) - 1):
+        if is_increasing and A[i] > A[i + 1]:
+            is_increasing = False
+            array_bounds["inc"].append(i)
+            array_bounds["dec"].append(i + 1)
+        elif not is_increasing and A[i] <= A[i + 1]:
+            is_increasing = True
+            array_bounds["dec"].append(i)
+            array_bounds["inc"].append(i + 1)
+    if is_increasing:
+        array_bounds["inc"].append(len(A) - 1)
+    else:
+        array_bounds["dec"].append(len(A) - 1)
+    result = []
+    min_heap = []
+    for i in range(0, len(array_bounds["inc"]), 2):
+        index = array_bounds["inc"][i]
+        heapq.heappush(min_heap, (A[index], "inc", i, index))
+    for i in range(1, len(array_bounds["dec"]), 2):
+        index = array_bounds["dec"][i]
+        heapq.heappush(min_heap,
+                       (A[index], "dec", i, index))
+    while min_heap:
+        valve, arr_type, arr_index, index = heapq.heappop(min_heap)
+        result.append(valve)
+        if arr_type == "inc":
+            bounds_arr = array_bounds["inc"]
+            max_i = bounds_arr[arr_index + 1]
+            if index + 1 <= max_i:
+                heapq.heappush(min_heap,
+                               (A[index + 1], "inc", arr_index, index + 1))
+        elif arr_type == "dec":
+            bounds_arr = array_bounds["dec"]
+            min_i = bounds_arr[arr_index - 1]
+            if index - 1 >= min_i:
+                heapq.heappush(min_heap,
+                               (A[index - 1], "dec", arr_index, index - 1))
+        else:
+            assert False, f"Unrecognised array type {arr_type}"
+    return result
+
 
 def sort_k_increasing_decreasing_array(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    return sort_k_increasing_decreasing_array_v1(A)
 
 
 if __name__ == '__main__':
--- epi_judge_python/tests/test_sort_increasing_decreasing_array.py
+++ epi_judge_python/tests/test_sort_increasing_decreasing_array.py
@@ -2,6 +2,7 @@ import random
 
 from tests.test_base import TestBase
 
+
 class TestSortIncreasingDecreasingArray(TestBase):
     def test_example1(self):
         k_sorted = [1, 3, 5, 4, 2, 0, 6, 9, 12, 10, 8]
@@ -11,17 +12,18 @@ class TestSortIncreasingDecreasingArray(TestBase):
             f"Failed for {k_sorted}")
 
     def test_random(self):
-        k_sorted = []
-        reverse = False
-        for _ in range(1, 21):
-            sz = random.randint(1, 10)
-            arr = []
-            for i in range(sz):
-                arr.append(random.randint(0, 2 * sz))
-            arr.sort(reverse=reverse)
-            reverse ^= True
-            k_sorted.extend(arr)
-        result = self.solve(k_sorted)
-        assert all(
-            result[i] <= result[i + 1] for i in range(len(result) - 1)), (
-            f"Failed for {k_sorted}")
+        for _ in range(1000):
+            k_sorted = []
+            reverse = False if random.randint(0, 1) == 0 else True
+            for _ in range(1, 21):
+                sz = random.randint(1, 10)
+                arr = []
+                for i in range(sz):
+                    arr.append(random.randint(0, 2 * sz))
+                arr.sort(reverse=reverse)
+                reverse ^= True
+                k_sorted.extend(arr)
+            result = self.solve(k_sorted)
+            assert all(
+                result[i] <= result[i + 1] for i in range(len(result) - 1)), (
+                f"Failed for {k_sorted}")
