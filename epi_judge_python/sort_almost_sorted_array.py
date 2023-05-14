3df9e6b19c365809c98472603e275803595d587c
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-14 17:44:07 +0530

    A1-python: Better implementation of sorting k-sorted array

--- epi_judge_python/sort_almost_sorted_array.py
+++ epi_judge_python/sort_almost_sorted_array.py
@@ -28,9 +28,33 @@ def sort_approximately_sorted_array_v1(sequence: Iterator[int],
     return result
 
 
+def sort_approximately_sorted_array_v2(sequence: Iterator[int],
+                                       k: int) -> List[int]:
+    '''
+    Program wise better version with similar runtime
+    '''
+    min_heap = []
+    index = 0
+    value = next(sequence, None)
+    while value is not None and index < k:
+        heapq.heappush(min_heap, value)
+        value = next(sequence, None)
+        index += 1
+    result = []
+    while value is not None:
+        min_so_far = heapq.heappushpop(min_heap, value)
+        result.append(min_so_far)
+        value = next(sequence, None)
+    while min_heap:
+        min_so_far = heapq.heappop(min_heap)
+        result.append(min_so_far)
+    return result
+
+
 def sort_approximately_sorted_array(sequence: Iterator[int],
                                     k: int) -> List[int]:
-    return sort_approximately_sorted_array_v1(sequence, k)
+    # return sort_approximately_sorted_array_v1(sequence, k)
+    return sort_approximately_sorted_array_v2(sequence, k)
 
 
 def sort_approximately_sorted_array_wrapper(sequence, k):
@@ -39,6 +63,7 @@ def sort_approximately_sorted_array_wrapper(sequence, k):
 
 if __name__ == '__main__':
     TestSortAlmostSortedArray(sort_approximately_sorted_array_v1).run_tests()
+    TestSortAlmostSortedArray(sort_approximately_sorted_array_v2).run_tests()
     exit(
         generic_test.generic_test_main(
             'sort_almost_sorted_array.py', 'sort_almost_sorted_array.tsv',
