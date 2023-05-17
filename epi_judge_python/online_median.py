23f8b15e5c4b25b327007acc73d92bf1f096f7f4
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-17 11:45:17 +0530

    A1-python: Codewise better version for running median problem

--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -8,7 +8,7 @@ from tests.test_online_median import TestOnlineMedian
 
 def online_median_v1(sequence: Iterator[int]) -> List[float]:
     '''
-    My attempt
+    My attempt with O(nlog(n)) for n entries
     '''
     min_heap = []
     max_heap = []
@@ -50,8 +50,34 @@ def online_median_v1(sequence: Iterator[int]) -> List[float]:
     return medians
 
 
+def online_median_v2(sequence: Iterator[int]) -> List[float]:
+    '''
+    Code wise better version with flipped conditional checking with same runtime
+    '''
+    max_heap = []
+    min_heap = []
+    medians = []
+    for value in sequence:
+        if len(max_heap) == len(min_heap):
+            if not min_heap or value <= min_heap[0]:
+                heapq.heappush(max_heap, -value)
+            else:
+                min_val = heapq.heapreplace(min_heap, value)
+                heapq.heappush(max_heap, -min_val)
+            medians.append(-max_heap[0])
+        else:
+            if value >= -max_heap[0]:
+                heapq.heappush(min_heap, value)
+            else:
+                max_val = -heapq.heapreplace(max_heap, -value)
+                heapq.heappush(min_heap, max_val)
+            medians.append((min_heap[0] - max_heap[0])/2)
+    return medians
+
+
 def online_median(sequence: Iterator[int]) -> List[float]:
-    return online_median_v1(sequence)
+    # return online_median_v1(sequence)
+    return online_median_v2(sequence)
 
 
 def online_median_wrapper(sequence):
@@ -60,6 +86,7 @@ def online_median_wrapper(sequence):
 
 if __name__ == '__main__':
     TestOnlineMedian(online_median_v1).run_tests()
+    TestOnlineMedian(online_median_v2).run_tests()
     exit(
         generic_test.generic_test_main('online_median.py', 'online_median.tsv',
                                        online_median_wrapper))
