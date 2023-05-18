fc475e8fae36d7cb019cc3d7e13f1aca4544fe73
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-18 15:57:25 +0530

    A1-python: Solved finding k-largest in a max heap

--- epi_judge_python/k_largest_in_heap.py
+++ epi_judge_python/k_largest_in_heap.py
@@ -1,3 +1,5 @@
+import heapq
+
 from typing import List
 
 from test_framework import generic_test, test_utils
@@ -5,12 +7,39 @@ from test_framework import generic_test, test_utils
 from tests.test_k_largest_in_heap import TestKLargestInHeap
 
 
+def k_largest_in_binary_heap_v1(A: List[int], k: int) -> List[int]:
+    '''
+    My version
+    '''
+    def get_children(n: int, size: int) -> List[int]:
+        children = []
+        if 2 * n + 1 < size:
+            children.append(2 * n + 1)
+        if 2 * n + 2 < size:
+            children.append(2 * n + 2)
+        return children
+    
+    min_heap = []
+    index_queue = [0]
+    while index_queue:
+        curr_index_queue = index_queue[:]
+        index_queue = []
+        for index in curr_index_queue:
+            if len(min_heap) < k:
+                heapq.heappush(min_heap, A[index])
+                index_queue.extend(get_children(index, len(A)))
+            elif len(min_heap) == k and A[index] > min_heap[0]:
+                heapq.heapreplace(min_heap, A[index])
+                index_queue.extend(get_children(index, len(A)))
+    return min_heap
+
+
 def k_largest_in_binary_heap(A: List[int], k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    return k_largest_in_binary_heap_v1(A, k)
 
 
 if __name__ == '__main__':
+    TestKLargestInHeap(k_largest_in_binary_heap_v1).run_tests()
     exit(
         generic_test.generic_test_main(
             'k_largest_in_heap.py',
