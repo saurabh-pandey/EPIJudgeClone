c92a01389a21514965efeee85609d931a3c7f7e6
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-19 12:22:51 +0530

    A1-python: Done with finding k-largest in a max heapified array

--- epi_judge_python/k_largest_in_heap.py
+++ epi_judge_python/k_largest_in_heap.py
@@ -9,15 +9,10 @@ from tests.test_k_largest_in_heap import TestKLargestInHeap
 
 def k_largest_in_binary_heap_v1(A: List[int], k: int) -> List[int]:
     '''
-    My version
+    My version with O(nlog(k)) runtime and O(n) space
     '''
     def get_children(n: int, size: int) -> List[int]:
-        children = []
-        if 2 * n + 1 < size:
-            children.append(2 * n + 1)
-        if 2 * n + 2 < size:
-            children.append(2 * n + 2)
-        return children
+        return [i for i in [2 * n + 1, 2 * n + 2] if i < size]
     
     min_heap = []
     index_queue = [0]
@@ -33,13 +28,36 @@ def k_largest_in_binary_heap_v1(A: List[int], k: int) -> List[int]:
                 index_queue.extend(get_children(index, len(A)))
     return min_heap
 
+def k_largest_in_binary_heap_v2(A: List[int], k: int) -> List[int]:
+    '''
+    Book's version with O(klog(k))
+    '''
+    if k <= 0:
+        return []
+    result = []
+    max_value_index_heap = [(-A[0], 0)]
+    for _ in range(k):
+        index = heapq.heappop(max_value_index_heap)[1]
+        result.append(A[index])
+        left_child_index = 2 * index + 1
+        if left_child_index < len(A):
+            heapq.heappush(max_value_index_heap,
+                           (-A[left_child_index], left_child_index))
+        right_child_index = 2 * index + 2
+        if right_child_index < len(A):
+            heapq.heappush(max_value_index_heap,
+                           (-A[right_child_index], right_child_index))
+    return result
+
 
 def k_largest_in_binary_heap(A: List[int], k: int) -> List[int]:
-    return k_largest_in_binary_heap_v1(A, k)
+    # return k_largest_in_binary_heap_v1(A, k)
+    return k_largest_in_binary_heap_v2(A, k)
 
 
 if __name__ == '__main__':
     TestKLargestInHeap(k_largest_in_binary_heap_v1).run_tests()
+    TestKLargestInHeap(k_largest_in_binary_heap_v2).run_tests()
     exit(
         generic_test.generic_test_main(
             'k_largest_in_heap.py',
