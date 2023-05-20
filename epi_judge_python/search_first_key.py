0d4ac57137c0f8efebd4c30cef652e60e460406b
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-20 15:41:04 +0530

    A1-python: Done with searching for the first occurance of a key in a sorted array

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -23,12 +23,32 @@ def search_first_of_k_v1(A: List[int], k: int) -> int:
     return result if result < len(A) else -1
 
 
+def search_first_of_k_v2(A: List[int], k:int) -> int:
+    '''
+    Book inspired version with same runtime and space complexity
+    '''
+    begin, end = 0, len(A) - 1
+    result = -1
+    while begin <= end:
+        mid = begin + (end - begin)//2
+        if k < A[mid]:
+            end = mid - 1
+        elif k == A[mid]:
+            result = mid
+            end = mid - 1
+        else:
+            begin = mid + 1
+    return result
+
+
 def search_first_of_k(A: List[int], k: int) -> int:
-    return search_first_of_k_v1(A, k)
+    # return search_first_of_k_v1(A, k)
+    return search_first_of_k_v2(A, k)
 
 
 if __name__ == '__main__':
     TestSearchFirstKey(search_first_of_k_v1).run_tests()
+    TestSearchFirstKey(search_first_of_k_v2).run_tests()
     exit(
         generic_test.generic_test_main('search_first_key.py',
                                        'search_first_key.tsv',
