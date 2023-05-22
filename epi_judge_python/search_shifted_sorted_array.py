695c73016bbaa5ecaaed8705ac5821cdf0cf67a4
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-22 16:14:39 +0530

    A1-python: Done with searching smallest in sorted and right shifted array

--- epi_judge_python/search_shifted_sorted_array.py
+++ epi_judge_python/search_shifted_sorted_array.py
@@ -7,7 +7,7 @@ from tests.test_search_shifted_sorted_array import TestSearchShiftedSortedArray
 
 def search_smallest_v1(A: List[int]) -> int:
     '''
-    My version
+    My version with O(log(n)) runtime
     '''
     begin, end = 0, len(A) - 1
     while begin < end:
@@ -19,12 +19,28 @@ def search_smallest_v1(A: List[int]) -> int:
     return begin
 
 
+def search_smallest_v2(A: List[int]) -> int:
+    '''
+    Book's version
+    '''
+    begin, end = 0, len(A) - 1
+    while begin < end:
+        mid = begin + (end - begin) // 2
+        if A[mid] > A[end]:
+            begin = mid + 1
+        else:
+            end = mid
+    return begin
+
+
 def search_smallest(A: List[int]) -> int:
-    return search_smallest_v1(A)
+    # return search_smallest_v1(A)
+    return search_smallest_v2(A)
 
 
 if __name__ == '__main__':
     TestSearchShiftedSortedArray(search_smallest_v1).run_tests()
+    TestSearchShiftedSortedArray(search_smallest_v2).run_tests()
     exit(
         generic_test.generic_test_main('search_shifted_sorted_array.py',
                                        'search_shifted_sorted_array.tsv',
