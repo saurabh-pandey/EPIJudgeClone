704e2d4dc34a336aa835cb13d7951b1832cbe451
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-23 13:49:13 +0530

    A1-python: Done with finding square root of an integer

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -5,7 +5,7 @@ from tests.test_int_square_root import TestIntSquareRoot
 
 def square_root_v1(k: int) -> int:
     '''
-    My version
+    My O(log(n)) version where n is the input number
     '''
     begin, end = 0, k
     while begin < end:
@@ -24,12 +24,30 @@ def square_root_v1(k: int) -> int:
             begin = mid
     return begin
 
+
+def square_root_v2(k: int) -> int:
+    '''
+    Book's version with same runtime
+    '''
+    begin, end = 0, k
+    while begin <= end:
+        mid = begin + (end - begin) // 2
+        square = mid * mid
+        if square <= k:
+            begin = mid + 1
+        else:
+            end = mid - 1
+    return begin - 1
+
+
 def square_root(k: int) -> int:
-    return square_root_v1(k)
+    # return square_root_v1(k)
+    return square_root_v2(k)
 
 
 if __name__ == '__main__':
     TestIntSquareRoot(square_root_v1).run_tests()
+    TestIntSquareRoot(square_root_v2).run_tests()
     exit(
         generic_test.generic_test_main('int_square_root.py',
                                        'int_square_root.tsv', square_root))
