2f84124a858a675f8582bd6e78f567b957426370
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-25 14:24:12 +0530

    A1-python: Preparing to solve finding kth largest element in an array aka kth order statistic

--- epi_judge_python/kth_largest_in_array.py
+++ epi_judge_python/kth_largest_in_array.py
@@ -2,6 +2,12 @@ from typing import List
 
 from test_framework import generic_test
 
+from tests.test_kth_largest_in_array import TestKthLargestInArray
+
+
+def find_kth_largest_v1(k: int, A: List[int]) -> int:
+    return 0
+
 
 # The numbering starts from one, i.e., if A = [3, 1, -1, 2]
 # find_kth_largest(1, A) returns 3, find_kth_largest(2, A) returns 2,
@@ -12,6 +18,7 @@ def find_kth_largest(k: int, A: List[int]) -> int:
 
 
 if __name__ == '__main__':
+    TestKthLargestInArray(find_kth_largest_v1).run_tests()
     exit(
         generic_test.generic_test_main('kth_largest_in_array.py',
                                        'kth_largest_in_array.tsv',
---
+++ epi_judge_python/tests/test_kth_largest_in_array.py
@@ -0,0 +1,24 @@
+import random
+
+from typing import List
+
+from tests.test_base import TestBase
+
+class TestKthLargestInArray(TestBase):
+    def test_example1(self):
+        A = [3, 2, 1, 5, 4]
+        self._check_result(A)
+    
+    def test_random(self):
+        for size in range(1, 1001):
+            A = [random.randint(0, 2 * size) for _ in range(size)]
+            self._check_result(A)
+    
+    def _check_result(self, A: List[int]) -> None:
+        descending_A = sorted(A, reverse=True)
+        for i, expected in enumerate(descending_A):
+            k = i + 1
+            result = self.solve(k, A)
+            assert result == expected, (
+                f"Expected = {expected}, result = {result}, k = {k}"
+            )
