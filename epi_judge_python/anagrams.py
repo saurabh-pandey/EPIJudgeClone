fe950893be4b64037caa434d5b13ea8c19706551
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-05-31 22:31:59 +0530

    A1-python: Initial working but very slow version of finding all anagrams

--- epi_judge_python/anagrams.py
+++ epi_judge_python/anagrams.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List
 
 from test_framework import generic_test, test_utils
 
@@ -32,16 +32,47 @@ def find_anagrams_v1(dictionary: List[str]) -> List[List[str]]:
     return anagrams
 
 
+# def find_anagrams_v2(dictionary: List[str]) -> List[List[str]]:
+#     '''
+#     My attempt
+#     '''
+#     def hash_word_distrib(word_distrib: Dict[str, int]) -> str:
+#         hash_value = []
+#         for c, v in word_distrib.items():
+#             hash_value.append(c)
+#             hash_value.append(str(v))
+#         return "".join(hash_value)
+#     word_dist_repo = {}
+#     anagrams = {}
+#     for word in dictionary:
+#         word_dist = {l: 0 for l in word}
+#         for l in word:
+#                 word_dist[l] += 1
+#         hashed_val = hash_word_distrib(word_dist)
+#         if hashed_val in word_dist_repo:
+#             index = word_dist_repo.index(word_dist)
+#             if index < len(anagrams):
+#                 anagrams[index].append(word)
+#             else:
+#                 print("Should not happen")
+#                 anagrams.append([word])
+#         else:
+#             word_dist_repo.append(word_dist)
+#             anagrams.append([word])
+#     anagrams[:] = [a for a in anagrams if len(a) > 1]
+#     return anagrams
+
+
+
 def find_anagrams(dictionary: List[str]) -> List[List[str]]:
-    # TODO - you fill in here.
-    return []
+    return find_anagrams_v1(dictionary)
 
 
 if __name__ == '__main__':
     TestAnagrams(find_anagrams_v1).run_tests()
-    # exit(
-    #     generic_test.generic_test_main(
-    #         'anagrams.py',
-    #         'anagrams.tsv',
-    #         find_anagrams,
-    #         comparator=test_utils.unordered_compare))
+    exit(
+        generic_test.generic_test_main(
+            'anagrams.py',
+            'anagrams.tsv',
+            find_anagrams,
+            comparator=test_utils.unordered_compare))
