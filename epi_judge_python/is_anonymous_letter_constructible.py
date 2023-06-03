d2bde4b657ba0711b49ea7f3932436fdd7a7bcb2
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-06-03 14:51:47 +0530

    A1-python: Solved checking if letter is constructible from magazine

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -2,14 +2,39 @@ import collections
 
 from test_framework import generic_test
 
+from tests.test_is_anonymous_letter_constructible import (
+    TestIsLetterConstructible)
+
+
+def is_letter_constructible_from_magazine_v1(letter_text: str,
+                                             magazine_text: str) -> bool:
+    '''
+    My version
+    '''
+    if not letter_text and not magazine_text:
+        return True
+    letter_char_freq = collections.Counter(letter_text)
+    magazine_char_counter = collections.Counter()
+    chars_found = set()
+    for c in magazine_text:
+        if c not in chars_found:
+            if c in letter_char_freq:
+                magazine_char_counter[c] += 1
+                if letter_char_freq[c] == magazine_char_counter[c]:
+                    chars_found.add(c)
+            if len(chars_found) == len(letter_char_freq):
+                return True
+    return False
+
 
 def is_letter_constructible_from_magazine(letter_text: str,
                                           magazine_text: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    return is_letter_constructible_from_magazine_v1(letter_text, magazine_text)
 
 
 if __name__ == '__main__':
+    TestIsLetterConstructible(
+        is_letter_constructible_from_magazine_v1).run_tests()
     exit(
         generic_test.generic_test_main(
             'is_anonymous_letter_constructible.py',
--- epi_judge_python/tests/test_is_anonymous_letter_constructible.py
+++ epi_judge_python/tests/test_is_anonymous_letter_constructible.py
@@ -5,57 +5,44 @@ import string
 from tests.test_base import TestBase
 
 
-class TestIsLetterConstructible(TestBase):
-    def test_ascii(self):
-        for letter_text, magazine_text in zip(
-                self._generate_text(string.ascii_letters, 10, 20),
-                self._generate_text(string.ascii_letters, 100, 200)):
-            letter_char_freq = collections.Counter(letter_text)
-            mag_char_freq = collections.Counter(magazine_text)
-            expected = any(
-                letter_char_freq[c] > mag_char_freq[c]
-                for c in letter_char_freq)
-            result = self.solve(letter_text, magazine_text)
-            assert expected == result, f"Expected {expected}, result {result}"
-    
-    def test_mismatch(self):
-        for letter_text, magazine_text in zip(
-                self._generate_text(string.printable, 10, 20),
-                self._generate_text(string.ascii_letters, 100, 200)):
-            letter_char_freq = collections.Counter(letter_text)
-            mag_char_freq = collections.Counter(magazine_text)
-            expected = any(
-                letter_char_freq[c] > mag_char_freq[c]
-                for c in letter_char_freq)
-            result = self.solve(letter_text, magazine_text)
-            assert expected == result, f"Expected {expected}, result {result}"
-    
-    def test_printable(self):
-        for letter_text, magazine_text in zip(
-                self._generate_text(string.printable, 10, 20),
-                self._generate_text(string.printable, 100, 200)):
-            letter_char_freq = collections.Counter(letter_text)
-            mag_char_freq = collections.Counter(magazine_text)
-            expected = any(
-                letter_char_freq[c] > mag_char_freq[c]
-                for c in letter_char_freq)
-            result = self.solve(letter_text, magazine_text)
-            assert expected == result, f"Expected {expected}, result {result}"
-    
+import pdb
 
-    def _generate_text(self,
-                       input_chars: str,
-                       min_size: int,
-                       max_size: int):
-        for unique_chars_len in range(10, len(input_chars)):
-            unique_chars = set()
-            while len(unique_chars) < unique_chars_len:
-                unique_chars.add(
-                    input_chars[random.randint(0, len(input_chars))])
-            letter_chars = list(unique_chars)
-            letter_text = []
-            for size in range(min_size, max_size):
-                for _ in range(size):
-                    letter_text.append(
-                        letter_chars[random.randint(0, len(letter_chars))])
-                yield "".join(letter_text)
+class TestIsLetterConstructible(TestBase):
+    def test_example1(self):
+        letter_text = "112"
+        magazine_text = "1123"
+        expected = self._check(letter_text, magazine_text)
+        result = self.solve(letter_text, magazine_text)
+        assert expected == result, f"expected {expected} result {result}"
+    
+    def test_example2(self):
+        letter_text = "112233"
+        magazine_text = "1123333445566"
+        expected = self._check(letter_text, magazine_text)
+        result = self.solve(letter_text, magazine_text)
+        assert expected == result, f"expected {expected} result {result}"
+    
+    def test_empty(self):
+        letter_text = ""
+        magazine_text = ""
+        expected = self._check(letter_text, magazine_text)
+        result = self.solve(letter_text, magazine_text)
+        assert expected == result, f"expected {expected} result {result}"
+    
+    def test_equal(self):
+        letter_text = "123"
+        magazine_text = "123"
+        expected = self._check(letter_text, magazine_text)
+        result = self.solve(letter_text, magazine_text)
+        assert expected == result, f"expected {expected} result {result}"
+
+    def _check(self, letter_text: str, magazine_text: str) -> bool:
+        letter_char_freq = collections.Counter(letter_text)
+        mag_char_freq = collections.Counter(magazine_text)
+        for c in letter_char_freq:
+            if c not in mag_char_freq:
+                return False
+            elif letter_char_freq[c] > mag_char_freq[c]:
+                return False
+        return True
+
