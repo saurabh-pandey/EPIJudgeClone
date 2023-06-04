654bc43acc93901a894962f6c92fcc9f8a1ef68f
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-06-04 12:11:37 +0530

    A1-python: Done with checking if letter is constructible from magazine

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -9,9 +9,10 @@ from tests.test_is_anonymous_letter_constructible import (
 def is_letter_constructible_from_magazine_v1(letter_text: str,
                                              magazine_text: str) -> bool:
     '''
-    My version
+    My version O(m + n) time with m and n being size of letter and magazine 
+    resp. O(c) space where c is number of unique chars in letter.
     '''
-    if not letter_text and not magazine_text:
+    if not letter_text:
         return True
     letter_char_freq = collections.Counter(letter_text)
     magazine_char_counter = collections.Counter()
@@ -27,14 +28,33 @@ def is_letter_constructible_from_magazine_v1(letter_text: str,
     return False
 
 
+def is_letter_constructible_from_magazine_v2(letter_text: str,
+                                             magazine_text: str) -> bool:
+    '''
+    Book's version with similar time and space complexity.
+    '''
+    letter_char_freq = collections.Counter(letter_text)
+    for c in magazine_text:
+        if c in letter_char_freq:
+            letter_char_freq[c] -= 1
+            if letter_char_freq[c] == 0:
+                del letter_char_freq[c]
+                if not letter_char_freq:
+                    return True
+    return not letter_char_freq
+
+
 def is_letter_constructible_from_magazine(letter_text: str,
                                           magazine_text: str) -> bool:
     return is_letter_constructible_from_magazine_v1(letter_text, magazine_text)
+    # return is_letter_constructible_from_magazine_v2(letter_text, magazine_text)
 
 
 if __name__ == '__main__':
     TestIsLetterConstructible(
         is_letter_constructible_from_magazine_v1).run_tests()
+    TestIsLetterConstructible(
+        is_letter_constructible_from_magazine_v2).run_tests()
     exit(
         generic_test.generic_test_main(
             'is_anonymous_letter_constructible.py',
