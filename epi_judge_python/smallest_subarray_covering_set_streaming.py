0e82e167bf0e89809d7f9434fa0e505aaa26e106
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-06-12 16:06:34 +0530

    A1-python: Done with smallest subarray covering a set of keywords for streaming input

--- epi_judge_python/smallest_subarray_covering_set_streaming.py
+++ epi_judge_python/smallest_subarray_covering_set_streaming.py
@@ -2,7 +2,7 @@ import collections
 import functools
 import sys
 
-from typing import Iterator, List, Set
+from typing import Any, Iterator, List, Set
 
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
@@ -18,7 +18,8 @@ def find_smallest_subarray_covering_set_streaming_v1(
         paragraph: Iterator[str],
         keywords: Set[str]) -> Subarray:
     '''
-    My version
+    My version with O(nk) where n is paragraph size and k is size of keywords
+    time and O(k) space
     '''
     start, end = -1, -1
     keyword_index = {k : [] for k in keywords}
@@ -46,9 +47,73 @@ def find_smallest_subarray_covering_set_streaming_v1(
     return start, end
 
 
+def find_smallest_subarray_covering_set_streaming_v2(
+        paragraph: Iterator[str],
+        keywords: Set[str]) -> Subarray:
+    '''
+    Book's version with O(n) time where n is paragraph size and k is size of keywords and O(k) space
+    '''
+    class Node:
+        def __init__(self, data: Any) -> None:
+            self.data: Any = data
+            self.next: 'Node' = None
+            self.prev: 'Node' = None
+    
+    class DoublyLinkedList:
+        def __init__(self) -> None:
+            self.head: Node = None
+            self.tail: Node = None
+            self._size: int = 0
+        
+        def __len__(self) -> int:
+            return self._size
+        
+        def insert_at_tail(self, value: int) -> None:
+            node = Node(value)
+            node.prev = self.tail
+            if self.tail:
+                self.tail.next = node
+            else:
+                self.head = node
+            self.tail = node
+            self._size += 1
+        
+        def remove(self, node: Node) -> None:
+            if node.next:
+                node.next.prev = node.prev
+            else:
+                self.tail = node.prev
+            if node.prev:
+                node.prev.next = node.next
+            else:
+                self.head = node.next
+            node.next = None
+            node.prev = None
+            self._size -= 1
+    
+    start, end = -1, -1
+    keyword_node_map = {k: None for k in keywords}
+    index_list = DoublyLinkedList()
+    for idx, word in enumerate(paragraph):
+        if word in keyword_node_map:
+            node = keyword_node_map[word]
+            if node:
+                index_list.remove(node)
+            index_list.insert_at_tail(idx)
+            keyword_node_map[word] = index_list.tail
+        if len(index_list) == len(keywords):
+            if (start, end) == (-1, -1):
+                start, end = index_list.head.data, idx
+            elif idx - index_list.head.data < end - start:
+                start, end = index_list.head.data, idx
+    return start, end
+
+
 def find_smallest_subarray_covering_set(paragraph: List[str],
                                         keywords: Set[str]) -> Subarray:
-    return find_smallest_subarray_covering_set_streaming_v1(
+    # return find_smallest_subarray_covering_set_streaming_v1(
+    #     iter(paragraph), keywords)
+    return find_smallest_subarray_covering_set_streaming_v2(
         iter(paragraph), keywords)
 
 
@@ -80,9 +145,18 @@ def find_smallest_subarray_covering_set_streaming_wrapper_v1(
         iter(paragraph), keywords)
 
 
+def find_smallest_subarray_covering_set_streaming_wrapper_v2(
+        paragraph: List[str],
+        keywords: Set[str]) -> Subarray:
+    return find_smallest_subarray_covering_set_streaming_v2(
+        iter(paragraph), keywords)
+
+
 if __name__ == '__main__':
     TestSmallestSubarrayCoveringSet(
         find_smallest_subarray_covering_set_streaming_wrapper_v1).run_tests()
+    TestSmallestSubarrayCoveringSet(
+        find_smallest_subarray_covering_set_streaming_wrapper_v2).run_tests()
     exit(
         generic_test.generic_test_main(
             'smallest_subarray_covering_set.py',
