09d5ad6752ccb133d251a789b135f488e6b1a085
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-06-13 14:15:39 +0530

    A1-python: Trying to solve finding smallest subarray covering all values

--- epi_judge_python/smallest_subarray_covering_all_values.py
+++ epi_judge_python/smallest_subarray_covering_all_values.py
@@ -1,8 +1,11 @@
+import bisect
 import collections
 import functools
+
 from typing import List
 
 from test_framework import generic_test
+from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 from tests.test_smallest_subarray_covering_all_values import (
@@ -17,14 +20,75 @@ def find_smallest_sequentially_covering_subset_v1(paragraph: List[str],
     '''
     My version
     '''
-    return (-1, -1)
+    def find_min_index(kw_indices, kw, index, value):
+        indices = kw_indices[kw[index]]
+        next_index = bisect.bisect_left(indices, value)
+        if next_index < len(indices) and indices[next_index] >= value:
+            return -1
+        new_value = (
+            indices[next_index] if next_index < len(indices) else indices[-1])
+        if index == 0:
+            return new_value
+        return find_min_index(kw_indices, kw, index - 1, new_value)
+            
+    kw_indices = {k : [] for k in keywords}
+    start, end = -1, -1
+    for i, word in enumerate(paragraph):
+        if word in kw_indices:
+            kw_indices[word].append(i)
+    for index in reversed(kw_indices[keywords[len(keywords) - 1]]):
+        min_index = find_min_index(kw_indices,
+                                   keywords,
+                                   len(keywords) - 2,
+                                   index)
+        if min_index >= 0:
+            if (start, end) == (-1, -1):
+              start, end = min_index, index
+            elif index - min_index < end - start:
+                start, end = min_index, index
+    return start, end
+
+
+
+# def find_smallest_sequentially_covering_subset_v2(paragraph: List[str],
+#                                                   keywords: List[str]
+#                                                   ) -> Subarray:
+#     '''
+#     My version
+#     '''
+#     kw_order = {k : i for i, k in enumerate(keywords)}
+#     kw_index = {k : -1 for k in keywords}
+#     start, end = -1, -1
+#     num_kw_found = 0
+#     current = 0
+#     for i, word in enumerate(paragraph):
+#         if word in kw_order:
+#             if kw_order[word] == current:
+#                 kw_index[word] = i
+#                 num_kw_found += 1
+#                 current += 1
+#                 if num_kw_found == len(kw_order):
+#                     if (start, end) == (-1, -1):
+#                         start, end = kw_index[keywords[0]], i
+#                     elif i - kw_index[keywords[0]] < end - start:
+#                         start, end = kw_index[keywords[0]], i
+#                     num_kw_found = 0
+#                     current = 0
+#             elif kw_order[word] == 0:
+#                 kw_index[word] = i
+#                 current = 1
+#                 num_kw_found = 1
+#             else:
+#                 current = 0
+#                 num_kw_found = 0
+#     return start, end
+#     # return Subarray(start, end)
 
 
 def find_smallest_sequentially_covering_subset(paragraph: List[str],
                                                keywords: List[str]
                                                ) -> Subarray:
-    # TODO - you fill in here.
-    return Subarray(0, 0)
+    return find_smallest_sequentially_covering_subset_v1(paragraph, keywords)
 
 
 @enable_executor_hook
@@ -37,6 +101,8 @@ def find_smallest_sequentially_covering_subset_wrapper(executor, paragraph,
     kw_idx = 0
     para_idx = result.start
     if para_idx < 0:
+        print(f"paragraph = {paragraph}")
+        print(f"keywords = {keywords}")
         raise RuntimeError('Subarray start index is negative')
 
     while kw_idx < len(keywords):
@@ -54,8 +120,8 @@ def find_smallest_sequentially_covering_subset_wrapper(executor, paragraph,
 if __name__ == '__main__':
     TestSmallestSubarrayCoveringAllValues(
         find_smallest_sequentially_covering_subset_v1).run_tests()
-    exit(
-        generic_test.generic_test_main(
-            'smallest_subarray_covering_all_values.py',
-            'smallest_subarray_covering_all_values.tsv',
-            find_smallest_sequentially_covering_subset_wrapper))
+    # exit(
+    #     generic_test.generic_test_main(
+    #         'smallest_subarray_covering_all_values.py',
+    #         'smallest_subarray_covering_all_values.tsv',
+    #         find_smallest_sequentially_covering_subset_wrapper))
--- epi_judge_python/tests/test_smallest_subarray_covering_all_values.py
+++ epi_judge_python/tests/test_smallest_subarray_covering_all_values.py
@@ -1,5 +1,7 @@
 from tests.test_base import TestBase
 
+import pdb
+
 class TestSmallestSubarrayCoveringAllValues(TestBase):
     def test_example1(self):
         paragraph = ["a", "b", "c", "a"]
@@ -11,6 +13,7 @@ class TestSmallestSubarrayCoveringAllValues(TestBase):
     def test_example2(self):
         paragraph = ["a", "c", "b", "a", "c", "a", "b"]
         keywords = ["a", "b"]
+        # pdb.set_trace()
         result = self.solve(paragraph, keywords)
         expected = (5, 6)
         assert result == expected, f"Expected {expected} != {result} result"
@@ -42,3 +45,12 @@ class TestSmallestSubarrayCoveringAllValues(TestBase):
         result = self.solve(paragraph, keywords)
         expected = (-1, -1)
         assert result == expected, f"Expected {expected} != {result} result"
+    
+    def test_attempt1(self):
+        paragraph =  ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '2',
+                      '4', '6', '1', '0', '1', '0', '1', '0', '3', '2', '1',
+                      '0']
+        keywords = ['0', '2', '9', '4', '6']
+        # pdb.set_trace()
+        result = self.solve(paragraph, keywords)
+        print("Res = ", result)
