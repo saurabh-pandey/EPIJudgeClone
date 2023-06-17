90b673edccd06c0545fa4a953c544db22d7c055b
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-06-17 12:56:22 +0530

    A1-python: Added some more tests for intersecting sorted arrays

--- epi_judge_python/tests/test_intersect_sorted_arrays.py
+++ epi_judge_python/tests/test_intersect_sorted_arrays.py
@@ -8,3 +8,32 @@ class TestIntersectSortedArrays(TestBase):
         expected = [5, 6, 8]
         result = self.solve(A, B)
         assert expected == result, f"Expected {expected} != {result} result"
+    
+    def test_no_intersection(self):
+        A = [1, 2, 3, 3, 4, 4]
+        B = [5, 5, 6, 8, 8, 9, 10, 10]
+        expected = []
+        result = self.solve(A, B)
+        assert expected == result, f"Expected {expected} != {result} result"
+    
+    def test_all_intersection(self):
+        A = [1, 2, 3, 3, 4, 4]
+        B = [1, 2, 3, 3, 4, 4]
+        expected = [1, 2, 3, 4]
+        result = self.solve(A, B)
+        assert expected == result, f"Expected {expected} != {result} result"
+    
+    def test_all_same(self):
+        A = [1, 1, 1, 1, 1, 1]
+        B = [1, 1, 1, 1, 1, 1]
+        expected = [1]
+        result = self.solve(A, B)
+        assert expected == result, f"Expected {expected} != {result} result"
+    
+    def test_all_diff(self):
+        A = [1, 1, 1, 1, 1, 1]
+        B = [2, 2, 2, 2, 2, 2]
+        expected = []
+        result = self.solve(A, B)
+        assert expected == result, f"Expected {expected} != {result} result"
+    
