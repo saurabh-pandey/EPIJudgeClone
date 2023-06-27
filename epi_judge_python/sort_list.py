05cb46f13340b0eb1a6195bc7e33004d8cee8152
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-06-27 13:22:21 +0530

    A1-python: Implemented merge sort for linked list

--- epi_judge_python/sort_list.py
+++ epi_judge_python/sort_list.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from list_node import ListNode
+from sorted_lists_merge import merge_two_sorted_lists_v2
 
 from test_framework import generic_test
 
@@ -52,14 +53,34 @@ def stable_sort_list_v2(L: ListNode) -> Optional[ListNode]:
     return dummy_head.next
 
 
+def stable_sort_list_v3(L: ListNode) -> Optional[ListNode]:
+    '''
+    O(n*log(n)) time and O(log(n)) space merge sort of a linked list
+    '''
+    if L is None or L.next is None:
+        return L
+    pre_slow, slow, fast = None, L, L
+    while fast and fast.next:
+        pre_slow, slow, fast = slow, slow.next, fast.next.next
+    if pre_slow:
+        pre_slow.next = None
+    
+    return merge_two_sorted_lists_v2(stable_sort_list_v3(L),
+                                     stable_sort_list_v3(slow))
+
+
+
+
 def stable_sort_list(L: ListNode) -> Optional[ListNode]:
     # return stable_sort_list_v1(L)
-    return stable_sort_list_v2(L)
+    # return stable_sort_list_v2(L)
+    return stable_sort_list_v3(L)
 
 
 if __name__ == '__main__':
     TestSortList(stable_sort_list_v1).run_tests()
     TestSortList(stable_sort_list_v2).run_tests()
+    TestSortList(stable_sort_list_v3).run_tests()
     exit(
         generic_test.generic_test_main('sort_list.py', 'sort_list.tsv',
                                        stable_sort_list))
