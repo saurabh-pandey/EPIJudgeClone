a4b6b34d9fe8b39550a1e1073c577c22da9a41c7
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-06-30 12:02:12 +0530

    A1-python: Implemented checking if a binary tree satisfies search tree property

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Tuple
 
 from binary_tree_node import BinaryTreeNode
 
@@ -10,24 +10,25 @@ def is_binary_tree_bst_v1(tree: BinaryTreeNode) -> bool:
     '''
     My version with O(n) time and O(h) space complexity
     '''
-    def check_bst_property(node: Optional[BinaryTreeNode],
-                           parent: Optional[BinaryTreeNode]) -> bool:
-        if not node:
-            return True
-        if node.left and node.data < node.left.data:
-            return False
-        if node.right and node.data > node.right.data:
-            return False
-        if parent:
-            if parent.left is node:
-                if node.right and parent.data < node.right.data:
-                    return False
-            else:
-                if node.left and parent.data > node.left.data:
-                    return False
-        return (check_bst_property(node.left, node)
-            and check_bst_property(node.right, node))
-    return check_bst_property(tree, None)
+    def check_bst_property(
+                node: Optional[BinaryTreeNode]
+            ) -> Tuple[bool, Optional[int], Optional[int]]:
+        if node is None:
+            return (True, None, None)
+        is_left_tree_bst, min_left, max_left = check_bst_property(node.left)
+        if not is_left_tree_bst:
+            return (False, None, None)
+        is_right_tree_bst, min_right, max_right = check_bst_property(node.right)
+        if not is_right_tree_bst:
+            return (False, None, None)
+        if max_left is not None and node.data < max_left:
+            return (False, None, None)
+        if min_right is not None and node.data > min_right:
+            return (False, None, None)
+        min_val = min_left if min_left is not None else node.data
+        max_val = max_right if max_right is not None else node.data
+        return (True, min_val, max_val)
+    return check_bst_property(tree)[0]
 
 
 def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
@@ -37,5 +38,6 @@ def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
 if __name__ == '__main__':
     TestIsTreeBst(is_binary_tree_bst_v1).run_tests()
     exit(
-        generic_test.generic_test_main('is_tree_a_bst.py', 'is_tree_a_bst.tsv',
+        generic_test.generic_test_main('is_tree_a_bst.py',
+                                       'is_tree_a_bst.tsv',
                                        is_binary_tree_bst))
--- epi_judge_python/tests/test_is_tree_a_bst.py
+++ epi_judge_python/tests/test_is_tree_a_bst.py
@@ -5,6 +5,7 @@ from tests.test_base import TestBase
 from tests.utils import binary_tree as btree
 from tests.utils import bst
 
+
 class TestIsTreeBst(TestBase):
     def test_example1(self):
         tree = bst.Factory.three_layered(BinaryTreeNode)
@@ -14,6 +15,12 @@ class TestIsTreeBst(TestBase):
     
     def test_example2(self):
         tree = btree.Factory.tree1()
+        result = self.solve(tree)
+        expected = False
+        assert result == expected, f"Expected {expected} != {result} result"
+    
+    def test_example3(self):
+        tree = btree.Factory.left_only()
         result = self.solve(tree)
         expected = False
         assert result == expected, f"Expected {expected} != {result} result"
