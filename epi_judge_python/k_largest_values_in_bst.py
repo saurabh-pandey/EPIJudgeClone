2d22307f331e27da8d15d7d09b829cabcf3a2533
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-07-04 10:15:11 +0530

    A1-python: Implemented finding k largest in a BST

--- epi_judge_python/k_largest_values_in_bst.py
+++ epi_judge_python/k_largest_values_in_bst.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from bst_node import BstNode
 
@@ -8,14 +8,28 @@ from tests.test_k_largest_values_in_bst import TestKLargestInBst
 
 def find_k_largest_in_bst_v1(tree: BstNode, k: int) -> List[int]:
     '''
-    My version
+    My version with O(k + h) time where k is number of values and h is the max
+    height. O(h) is space.
     '''
-    return []
+    def reverse_inorder(node: Optional[BstNode], 
+                        k: int,
+                        result: List[int]) -> bool:
+        if node is None:
+            return False
+        do_stop = reverse_inorder(node.right, k, result)
+        if do_stop:
+            return True
+        result.append(node.data)
+        if len(result) == k:
+            return True
+        return reverse_inorder(node.left, k, result)
+    result = []
+    reverse_inorder(tree, k, result)
+    return result
 
 
 def find_k_largest_in_bst(tree: BstNode, k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    return find_k_largest_in_bst_v1(tree, k)
 
 
 if __name__ == '__main__':
