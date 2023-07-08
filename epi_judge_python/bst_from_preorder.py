bd259e01c58ed1c2634ff6ca3ec3eb5e84ef36b7
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-07-08 16:52:45 +0530

    A1-python: Done constructing BST from preorder sequence

--- epi_judge_python/bst_from_preorder.py
+++ epi_judge_python/bst_from_preorder.py
@@ -1,3 +1,5 @@
+import sys
+
 from typing import List, Optional
 
 from bst_node import BstNode
@@ -44,15 +46,37 @@ def rebuild_bst_from_preorder_v2(
                         preorder_sequence[left_subarr_end:]))
 
 
+def rebuild_bst_from_preorder_v3(
+        preorder_sequence: List[int]) -> Optional[BstNode]:
+    '''
+    Book's O(n) time version
+    '''
+    def rebuild_bst_from_preorder_with_bounds(
+            lower_bound: int, upper_bound: int) -> Optional[BstNode]:
+        if root_idx[0] >= len(preorder_sequence):
+            return None
+        root = preorder_sequence[root_idx[0]]
+        if root < lower_bound or root > upper_bound:
+            return None
+        root_idx[0] += 1
+        left_subtree = rebuild_bst_from_preorder_with_bounds(lower_bound, root)
+        right_subtree = rebuild_bst_from_preorder_with_bounds(root, upper_bound)
+        return BstNode(root, left_subtree, right_subtree)    
+    root_idx = [0]
+    return rebuild_bst_from_preorder_with_bounds(-sys.maxsize, sys.maxsize)
+
+
 def rebuild_bst_from_preorder(preorder_sequence: List[int]
                               ) -> Optional[BstNode]:
     # return rebuild_bst_from_preorder_v1(preorder_sequence)
-    return rebuild_bst_from_preorder_v2(preorder_sequence)
+    # return rebuild_bst_from_preorder_v2(preorder_sequence)
+    return rebuild_bst_from_preorder_v3(preorder_sequence)
 
 
 if __name__ == '__main__':
     TestBstFromPreorder(rebuild_bst_from_preorder_v1).run_tests()
     TestBstFromPreorder(rebuild_bst_from_preorder_v2).run_tests()
+    TestBstFromPreorder(rebuild_bst_from_preorder_v3).run_tests()
     exit(
         generic_test.generic_test_main('bst_from_preorder.py',
                                        'bst_from_preorder.tsv',
