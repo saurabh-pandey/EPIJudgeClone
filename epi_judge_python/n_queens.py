865c5f38c370dd09415bd8f0c12a8d1c75ebf311
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-07-21 10:41:48 +0530

    A1-python: Another simpler move validity check for N Queens problem

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -56,8 +56,45 @@ def n_queens_v1(n: int) -> List[List[int]]:
     return solutions
 
 
+def n_queens_v2(n: int) -> List[List[int]]:
+    '''
+    My another version with simpler move validity check
+    '''
+    def is_valid_move(row: int, col: int) -> bool:
+        # All rows must have unique value
+        unique_rows = set(queens)
+        if row in unique_rows:
+            return False
+        # Check if this is falling on a diagonal of a queen
+        diag_row_positive, diag_row_negative = row + 1, row - 1
+        col_index = col - 1
+        while (col_index >= 0
+               and diag_row_positive < n or diag_row_negative >= 0):
+            if (queens[col_index] == diag_row_positive
+                or queens[col_index] == diag_row_negative):
+                return False
+            col_index -= 1
+            diag_row_positive += 1
+            diag_row_negative -= 1
+        return True
+    def n_queens_recursive(col: int) -> None:
+        if col == n:
+            solutions.append(queens[:])
+        else:
+            for row in range(n):
+                if is_valid_move(row, col):
+                    queens[col] = row
+                    n_queens_recursive(col + 1)
+                    queens[col] = -1
+    solutions = []
+    queens = [-1] * n
+    n_queens_recursive(0)
+    return solutions
+
+
 def n_queens(n: int) -> List[List[int]]:
-    return n_queens_v1(n)
+    # return n_queens_v1(n)
+    return n_queens_v2(n)
 
 
 def comp(a, b):
@@ -66,6 +103,7 @@ def comp(a, b):
 
 if __name__ == '__main__':
     TestNQueens(n_queens_v1).run_tests()
+    TestNQueens(n_queens_v2).run_tests()
     exit(
         generic_test.generic_test_main('n_queens.py', 'n_queens.tsv', n_queens,
                                        comp))
