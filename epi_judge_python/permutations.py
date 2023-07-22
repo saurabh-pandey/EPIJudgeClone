d8d55550ce8515f99e45c616f06c9b697f48d856
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-07-22 16:47:31 +0530

    A1-python: Preparing to solve permutations

--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -2,6 +2,15 @@ from typing import List
 
 from test_framework import generic_test, test_utils
 
+from tests.test_permutations import TestPermutations
+
+
+def permutations_v1(A: List[int]) -> List[List[int]]:
+    '''
+    My version
+    '''
+    return []
+
 
 def permutations(A: List[int]) -> List[List[int]]:
     # TODO - you fill in here.
@@ -9,6 +18,7 @@ def permutations(A: List[int]) -> List[List[int]]:
 
 
 if __name__ == '__main__':
+    TestPermutations(permutations_v1).run_tests()
     exit(
         generic_test.generic_test_main('permutations.py', 'permutations.tsv',
                                        permutations,
---
+++ epi_judge_python/tests/test_permutations.py
@@ -0,0 +1,25 @@
+import itertools
+
+from typing import List
+
+from tests.test_base import TestBase
+
+
+class TestPermutations(TestBase):
+    def test_example1(self):
+        A = [1, 2, 3]
+        expected = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2],
+                    [3, 2, 1]]
+        result = self.solve(A)
+        assert expected == result, f"Expected {expected} != {result} result"
+    
+    def test_all(self):
+        for n in range(1, 10):
+            A = list(range(n))
+            result = self.solve(A)
+            self.check_permutations(A, result)
+    
+    def check_permutations(self, A: List[int], result: List[List[int]]):
+        expected: List[List[int]] = [list(p) for p in itertools.permutations(A)]
+        result.sort()
+        assert expected == result, f"Expected {expected} != {result} result"
