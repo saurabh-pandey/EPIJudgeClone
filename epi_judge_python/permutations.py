f443be80b398cd454bcc3a817c2b4039d5a7bf3a
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-07-23 20:52:54 +0530

    A1-python: Solved finding all permutations

--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -9,12 +9,26 @@ def permutations_v1(A: List[int]) -> List[List[int]]:
     '''
     My version
     '''
-    return []
+    def permutations_recursive(count: int) -> None:
+        if count == len(A):
+            permutations.append(permutation[:])
+            return
+        for i, a in enumerate(A):
+            if i not in looked_indices:
+                looked_indices.add(i)
+                permutation[count] = a
+                permutations_recursive(count + 1)
+                looked_indices.remove(i)
+                permutation[count] = 0
+    permutations = []
+    permutation = [0] * len(A)
+    looked_indices = set()
+    permutations_recursive(0)
+    return permutations
 
 
 def permutations(A: List[int]) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    return permutations_v1(A)
 
 
 if __name__ == '__main__':
