23aa6820c6db90f9f03fc8ab61d0fb8489549fa5
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-07-31 09:15:10 +0530

    A1-python: Done with power set

--- epi_judge_python/power_set.py
+++ epi_judge_python/power_set.py
@@ -1,3 +1,5 @@
+import math
+
 from typing import List
 
 from test_framework import generic_test, test_utils
@@ -58,16 +60,34 @@ def generate_power_set_v3(input_set: List[int]) -> List[List[int]]:
     return power_set
 
 
+def generate_power_set_v4(input_set: List[int]) -> List[List[int]]:
+    '''
+    Book's final version
+    '''
+    power_set: List[List[int]] = []
+    for int_for_subset in range(1 << len(input_set)):
+        bit_array = int_for_subset
+        subset = []
+        while bit_array:
+            subset.append(input_set[int(math.log2(bit_array
+                                                  & ~(bit_array - 1)))])
+            bit_array &= (bit_array - 1)
+        power_set.append(subset)
+    return power_set
+
+
 def generate_power_set(input_set: List[int]) -> List[List[int]]:
     # return generate_power_set_v1(input_set)
     # return generate_power_set_v2(input_set)
-    return generate_power_set_v3(input_set)
+    # return generate_power_set_v3(input_set)
+    return generate_power_set_v4(input_set)
 
 
 if __name__ == '__main__':
     TestPowerSet(generate_power_set_v1).run_tests()
     TestPowerSet(generate_power_set_v2).run_tests()
     TestPowerSet(generate_power_set_v3).run_tests()
+    TestPowerSet(generate_power_set_v4).run_tests()
     exit(
         generic_test.generic_test_main('power_set.py', 'power_set.tsv',
                                        generate_power_set,
