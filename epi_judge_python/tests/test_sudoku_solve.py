0797f01986ea248234f2082349a232cbece97f46
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-08-01 15:59:28 +0530

    A1-python: Added sudoku solution testing

--- epi_judge_python/tests/test_sudoku_solve.py
+++ epi_judge_python/tests/test_sudoku_solve.py
@@ -2,7 +2,7 @@ import copy
 
 from enum import Enum
 from random import sample
-from typing import List
+from typing import List, Tuple
 
 from tests.test_base import TestBase
 
@@ -16,13 +16,24 @@ class Level(Enum):
 class TestSudokuSolve(TestBase):
     base: int = 3
     side: int = base * base
+    EMPTY: int = 0
+    sub_grid: List[Tuple[int, int]] = [
+        (0, 0), (0, 3), (0, 6),
+        (3, 0), (3, 3), (3, 6),
+        (6, 0), (6, 3), (6, 6),
+    ]
 
     
     def test_example1(self):
         solved_sudoku = self.generate_solved_sudoku()
-        self.generate_sudoku_puzzle(solved_sudoku)
-        self.generate_sudoku_puzzle(solved_sudoku, Level.MEDIUM)
-        self.generate_sudoku_puzzle(solved_sudoku, Level.HARD)
+        sudoku_puzzle_easy = self.generate_sudoku_puzzle(solved_sudoku)
+        print(f"Easy is solved = {self.is_solved(sudoku_puzzle_easy, solved_sudoku)}")
+        sudoku_puzzle_med = self.generate_sudoku_puzzle(solved_sudoku,
+                                                        Level.MEDIUM)
+        print(f"Medium is solved = {self.is_solved(sudoku_puzzle_med, solved_sudoku)}")
+        sudoku_puzzle_hard = self.generate_sudoku_puzzle(solved_sudoku,
+                                                         Level.HARD)
+        print(f"Hard is solved = {self.is_solved(sudoku_puzzle_hard, solved_sudoku)}")
 
         # sudoku_puzzle = self.generate_sudoku_puzzle()
         # result = self.solve(sudoku_puzzle)
@@ -60,13 +71,43 @@ class TestSudokuSolve(TestBase):
         for p in sample(range(squares), empties):
             i = p // TestSudokuSolve.side
             j = p % TestSudokuSolve.side
-            sudoku_puzzle[i][j] = 0
-
+            sudoku_puzzle[i][j] = TestSudokuSolve.EMPTY
         numSize = len(str(TestSudokuSolve.side))
         for line in sudoku_puzzle:
             print(*(f"{n or '.':{numSize}} " for n in line))
+        return sudoku_puzzle
 
     def is_solved(self,
                   puzzle: List[List[int]],
                   solution: List[List[int]]) -> bool:
-        pass
+        def is_puzzle_solution() -> bool:
+            for row in range(TestSudokuSolve.side):
+                for col in range(TestSudokuSolve.side):
+                    if puzzle[row][col] != TestSudokuSolve.EMPTY:
+                        if puzzle[row][col] != solution[row][col]:
+                            return False
+            return True
+        def is_row_correct(row) -> bool:
+            return len(set(solution[row])) == TestSudokuSolve.side
+        def is_col_correct(col) -> bool:
+            unique = set()
+            for i in range(TestSudokuSolve.side):
+                unique.add(solution[i][col])
+            return len(unique) == TestSudokuSolve.side
+        def is_subgroup_correct(group) -> bool:
+            unique = set()
+            row_start, col_start = TestSudokuSolve.sub_grid[group]
+            for row in range(row_start, row_start + TestSudokuSolve.base):
+                for col in range(col_start, col_start + TestSudokuSolve.base):
+                    unique.add(solution[row][col])
+            return len(unique) == TestSudokuSolve.side
+        if not is_puzzle_solution():
+            return False
+        for i in range(TestSudokuSolve.side):
+            if not is_row_correct(i):
+                return False
+            if not is_col_correct(i):
+                return False
+            if not is_subgroup_correct(i):
+                return False
+        return True
