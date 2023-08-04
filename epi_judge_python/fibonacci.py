0bcb563d02dd7f1e904803c62f227a0db4b4c017
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-08-04 19:15:43 +0530

    A1-python: Done with fibonacci

--- epi_judge_python/fibonacci.py
+++ epi_judge_python/fibonacci.py
@@ -24,13 +24,26 @@ def cached_fibonacci_v1(n: int) -> int:
     return cached_fibonacci_v1(n - 1) + cached_fibonacci_v1(n - 2)
 
 
+def fibonacci_v2(n: int) -> int:
+    '''
+    Iterative fibonacci
+    '''
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+
 def fibonacci(n: int) -> int:
     # return fibonacci_v1(n)
-    return cached_fibonacci_v1(n)
+    # return cached_fibonacci_v1(n)
+    return fibonacci_v2(n)
 
 
 if __name__ == '__main__':
     TestFibonacci(fibonacci_v1).run_tests()
+    TestFibonacci(cached_fibonacci_v1).run_tests()
+    TestFibonacci(fibonacci_v2).run_tests()
     exit(
         generic_test.generic_test_main('fibonacci.py', 'fibonacci.tsv',
                                        fibonacci))
