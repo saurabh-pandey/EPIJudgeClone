6330d7afe22433dcd1f71c41d9b017dd6440c86f
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-08-05 17:39:33 +0530

    A1-python: Better maximum subarray sum using pre-computed summation values

--- epi_judge_python/max_sum_subarray.py
+++ epi_judge_python/max_sum_subarray.py
@@ -9,7 +9,7 @@ from tests.test_max_sum_subarray import TestMaxSumSubarray
 
 def find_maximum_subarray_v1(A: List[int]) -> int:
     '''
-    Brute-force version
+    Brute-force version with O(n^3) time
     '''
     max_sum = 0
     for i in range(len(A)):
@@ -20,12 +20,31 @@ def find_maximum_subarray_v1(A: List[int]) -> int:
     return max_sum
 
 
+def find_maximum_subarray_v2(A: List[int]) -> int:
+    '''
+    O(n^2) version
+    '''
+    max_sum = 0
+    sum_array = [0 for _ in range(len(A) + 1)]
+    for i, a in enumerate(A):
+        sum_array[i + 1] = sum_array[i] + a
+    for i in range(len(A)):
+        for j in range(i, len(A)):
+            subarray_sum = sum_array[j + 1] - sum_array[i]
+            if subarray_sum > max_sum:
+                max_sum = subarray_sum
+    return max_sum
+
+
+
 def find_maximum_subarray(A: List[int]) -> int:
-    return find_maximum_subarray_v1(A)
+    # return find_maximum_subarray_v1(A)
+    return find_maximum_subarray_v2(A)
 
 
 if __name__ == '__main__':
     TestMaxSumSubarray(find_maximum_subarray_v1).run_tests()
+    TestMaxSumSubarray(find_maximum_subarray_v2).run_tests()
     exit(
         generic_test.generic_test_main('max_sum_subarray.py',
                                        'max_sum_subarray.tsv',
